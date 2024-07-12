c7504f49c35a74179ec09e11fd2d4f2e3ebd8e98
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-07-12 09:52:28 +0900

    chore: 초기 세팅 수정

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,9 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from '@testing-library/react';
 
-import Button from "./";
+import Button from './';
 
-test("renders the Button component with correct text", () => {
-  const buttonText = "렛서 판다 붐은 온다";
+test('renders the Button component with correct text', () => {
+  const buttonText = '렛서 판다 붐은 온다';
   render(<Button />);
 
   const buttonElement = screen.getByText(buttonText);
