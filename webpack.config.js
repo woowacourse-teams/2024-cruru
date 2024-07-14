2bc567c0300783cb2ac9dc4a8178534abf3dd983
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-07-14 10:28:38 +0900

    chore: 세팅 피드백 반영

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,61 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: "./src/main.tsx",
+  entry: './src/main.tsx',
   module: {
     rules: [
       {
-        test: /\.(js|jsx|ts|tsx|mjs)$/,
-        exclude: /node_modules/,
-        loader: "esbuild-loader",
-        options: {
-          target: "es2020",
+        test: /\.(png|svg|jpe?g|gif|webp)$/i,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 4 * 1024,
+          },
+        },
+        generator: {
+          filename: 'assets/images/[name]_[contenthash:8][ext]',
         },
       },
       {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: ["@svgr/webpack"],
+        test: /\.(js|jsx|ts|tsx|mjs)$/,
+        exclude: /node_modules/,
+        loader: 'esbuild-loader',
+        options: {
+          target: 'es2020',
+        },
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".mjs"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.mjs'],
     alias: {
-      "@": path.resolve(__dirname, "src/"),
-      "@components": path.resolve(__dirname, "src/components/"),
-      "styled-system": path.resolve(__dirname, "src/styled-system/"),
+      '@': path.resolve(__dirname, 'src/'),
+      '@components': path.resolve(__dirname, 'src/components/'),
+      '@assets': path.resolve(__dirname, 'src/assets/'),
+      'styled-system': path.resolve(__dirname, 'src/styled-system/'),
     },
   },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name]_[chunkhash:8].js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html",
-      filename: "index.html",
+      template: './index.html',
+      filename: 'index.html',
     }),
     new ForkTsCheckerWebpackPlugin({
       async: false,
       typescript: {
-        configFile: path.resolve(__dirname, "tsconfig.json"),
+        configFile: path.resolve(__dirname, 'tsconfig.json'),
       },
     }),
     new Dotenv(),
