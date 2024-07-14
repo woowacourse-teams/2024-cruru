964e8c49e695153c419c89c0c20dceb738af414e
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-07-14 11:10:11 +0900

    chore: eslint import rule 추가

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 
-import Button from './';
+import Button from '.';
 
 test('renders the Button component with correct text', () => {
   const buttonText = '렛서 판다 붐은 온다';
