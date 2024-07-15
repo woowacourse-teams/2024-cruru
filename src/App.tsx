6c4a7530383a9a65b6ea9cf58fc2ab51eec6052b
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-07-15 22:07:45 +0900

    chore: pandacss 설정 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,5 @@
-import { styled } from 'styled-system/jsx';
-import cat from '@/assets/images/cat.svg';
-
-const StyledDiv = styled.div`
-  background-color: red;
-  width: 100px;
-`;
+import ApplicantCard from './components/ApplicantCard';
 
 export default function App() {
-  return (
-    <StyledDiv className="app">
-      <img
-        src={cat}
-        alt="cat"
-      />
-      렛서 판다 붐은 온다
-    </StyledDiv>
-  );
+  return <ApplicantCard />;
 }
