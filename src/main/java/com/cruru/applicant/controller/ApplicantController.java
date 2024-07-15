a15a1e988846550bc652b26b06ae3ee9a2c0980e
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2024-07-15 22:51:37 +0900

    refactor: API path 컨벤션에 맞게 변경

--- src/main/java/com/cruru/applicant/controller/ApplicantController.java
+++ src/main/java/com/cruru/applicant/controller/ApplicantController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/applicants")
+@RequestMapping("/api/v1/applicants")
 @RequiredArgsConstructor
 public class ApplicantController {
 
