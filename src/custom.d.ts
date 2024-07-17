f2e37b56d4bc17f04c598fe70b701aaabb4a5151
DobbyMac <kimdobby@g.skku.edu>
2024-07-17 14:14:54 +0900

    revert: 잘못 적용된 "Merge branch 'fe/develop' into main" 커밋 revert
    This reverts commit 0fd8a207d712915bd3d8ae58c8220c2e557337d1, reversing
    changes made to bdf469f1d285278f3d2497990eebf15757dbd52e.

--- src/custom.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare module '*.svg' {
-  const content: string & {
-    ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  };
-  export default content;
-}
--- src/utils/formatDate.ts
+++
@@ -1,9 +0,0 @@
-export default function formatDate(dateString: string) {
-  const date = new Date(dateString);
-
-  const year = date.getFullYear().toString().slice(-2);
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-
-  return `${year}. ${month}. ${day}`;
-}
