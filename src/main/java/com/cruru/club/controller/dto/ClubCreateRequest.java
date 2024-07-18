c3020cdc96adbf0758c9bc149b6212827fc1ff15
leetaehoon <xogns1514@gmail.com>
2024-07-18 22:45:32 +0900

    refactor: 검증 메시지 추가

--- src/main/java/com/cruru/club/controller/dto/ClubCreateRequest.java
+++ src/main/java/com/cruru/club/controller/dto/ClubCreateRequest.java
@@ -2,6 +2,6 @@ package com.cruru.club.controller.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record ClubCreateRequest(@NotBlank String name) {
+public record ClubCreateRequest(@NotBlank(message = "동아리 이름은 필수 값입니다.") String name) {
 
 }
