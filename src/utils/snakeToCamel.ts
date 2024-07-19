4dc51539d0e2cf038754a8e111b0d6ca5d4d8a38
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-07-19 16:51:16 +0900

    feat: snakeToCamel 수정

--- src/utils/snakeToCamel.ts
+++ src/utils/snakeToCamel.ts
@@ -1,25 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-else-return */
+const toCamelCase = (str: string): string => str.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
 
-const toCamelCase = (str: string): string => {
-    return str.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
-  };
-  
-  /**
-   * 사용 예시 const camelCaseObject = keysToCamelCase(snakeCaseObject);
-   * @param obj 
-   * @returns 
-   */
- export default function keysToCamelCase (obj: any): any {
-    if (Array.isArray(obj)) {
-      return obj.map((item) => keysToCamelCase(item));
-    } else if (obj !== null && obj.constructor === Object) {
-      const newObj: any = {};
-      Object.keys(obj).forEach((key) => {
-        newObj[toCamelCase(key)] = keysToCamelCase(obj[key]);
-      });
-      return newObj;
-    }
-    return obj;
-  };
-  
-
-
+/**
+ * 사용 예시 const camelCaseObject = keysToCamelCase(snakeCaseObject);
+ * @param obj
+ * @returns
+ */
+export default function snakeToCamel(obj: any): any {
+  if (Array.isArray(obj)) {
+    return obj.map((item) => snakeToCamel(item));
+  } else if (obj !== null && obj.constructor === Object) {
+    const newObj: any = {};
+    Object.keys(obj).forEach((key) => {
+      newObj[toCamelCase(key)] = snakeToCamel(obj[key]);
+    });
+    return newObj;
+  }
+  return obj;
+}
