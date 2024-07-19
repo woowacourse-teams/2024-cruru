526845510889139c3f5f20e089a7cd0f01e3645e
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-07-19 17:19:34 +0900

    feat: 공통 모달 구현

---
+++ src/components/BaseModal/styled.ts
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled';
+
+const ModalOverlay = styled.dialog`
+  max-width: 100%;
+  max-height: 100%;
+  min-width: 300px;
+
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+  padding: 0;
+
+  border: 1px solid ${({ theme }) => theme.baseColors.grayscale[400]};
+  border-radius: 8px;
+
+  width: fit-content;
+  height: fit-content;
+
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  overflow: hidden;
+
+  &::backdrop {
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const S = {
+  ModalOverlay,
+};
+
+export default S;
