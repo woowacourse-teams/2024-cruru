f62168dc9da1d26f3fa1fa73e25cf4f21a1d684b
Seongjin Hong <hongseongjin.to@gmail.com>
2024-07-19 14:38:19 +0900

    chore: 경로 alias 설정 추가 및 일치화

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -20,9 +20,16 @@ const config: StorybookConfig = {
       config.resolve.alias = {
         ...config.resolve?.alias,
         '@': path.resolve(__dirname, '../src/'),
-        '@components': path.resolve(__dirname, '../src/components/'),
-        '@assets': path.resolve(__dirname, '../src/assets/'),
         '@api': path.resolve(__dirname, '../src/api/'),
+        '@assets': path.resolve(__dirname, '../src/assets/'),
+        '@components': path.resolve(__dirname, '../src/components/'),
+        '@constants': path.resolve(__dirname, 'src/constants/'),
+        '@contexts': path.resolve(__dirname, 'src/contexts/'),
+        '@hooks': path.resolve(__dirname, 'src/hooks/'),
+        '@pages': path.resolve(__dirname, 'src/pages/'),
+        '@styles': path.resolve(__dirname, 'src/styles/'),
+        '@types': path.resolve(__dirname, 'src/types/'),
+        '@utils': path.resolve(__dirname, 'src/utils/'),
       };
     }
     return config;
--- webpack.config.js
+++ webpack.config.js
@@ -36,9 +36,16 @@ module.exports = {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.mjs'],
     alias: {
       '@': path.resolve(__dirname, 'src/'),
-      '@components': path.resolve(__dirname, 'src/components/'),
-      '@assets': path.resolve(__dirname, 'src/assets/'),
       '@api': path.resolve(__dirname, 'src/api/'),
+      '@assets': path.resolve(__dirname, 'src/assets/'),
+      '@components': path.resolve(__dirname, 'src/components/'),
+      '@constants': path.resolve(__dirname, 'src/constants/'),
+      '@contexts': path.resolve(__dirname, 'src/contexts/'),
+      '@hooks': path.resolve(__dirname, 'src/hooks/'),
+      '@pages': path.resolve(__dirname, 'src/pages/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@types': path.resolve(__dirname, 'src/types/'),
+      '@utils': path.resolve(__dirname, 'src/utils/'),
     },
   },
   output: {
