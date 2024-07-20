63e709b8a53d83253eae7fe65bb6c86234e942e7
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-07-20 22:46:49 +0900

    feat: React Router 설정 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import ApplicantManage from './pages/ApplicantManage';
-
-const queryClient = new QueryClient();
+import { Outlet } from 'react-router-dom';
 
 export default function App() {
-  const router = createBrowserRouter(
-    [
-      {
-        path: '/',
-        element: <ApplicantManage />,
-      },
-    ],
-    {
-      basename: '/', //TODO: 배포할때 해당 루트로 적기
-    },
-  );
-
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       {/*Header, SideBar를 여기에 추가하면 됩니다. */}
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+      <Outlet />
+    </>
   );
 }
