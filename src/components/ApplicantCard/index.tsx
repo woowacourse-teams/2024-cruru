408abf0afc6e0a9b74904a37f8d2a30c301f7d64
Seongjin Hong <hongseongjin.to@gmail.com>
2024-07-21 16:43:39 +0900

    feat: ApplicantCard에서 PopOverMenu 외부 클릭시 닫히는 기능 추가

--- src/components/ApplicantCard/index.tsx
+++ src/components/ApplicantCard/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 import EllipsisIcon from '@assets/icons/ellipsis.svg';
 import S from './style';
@@ -15,9 +15,29 @@ interface ApplicantCardProps {
 }
 
 export default function ApplicantCard({ name, createdAt, popOverMenuItems }: ApplicantCardProps) {
-  const [isPopOverMenuOpen, setIsPopOverMenuOpen] = useState<boolean>(false);
+  const [isPopOverOpen, setIsPopOverOpen] = useState<boolean>(false);
+  const optionButtonWrapperRef = useRef<HTMLDivElement>(null);
 
-  const togglePopOverMenu = () => setIsPopOverMenuOpen((prevState) => !prevState);
+  const handleClickPopOverButton = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    setIsPopOverOpen((prevState) => !prevState);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (optionButtonWrapperRef.current && !optionButtonWrapperRef.current.contains(event.target as Node)) {
+      setIsPopOverOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isPopOverOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isPopOverOpen]);
 
   return (
     <S.CardContainer>
@@ -25,22 +45,25 @@ export default function ApplicantCard({ name, createdAt, popOverMenuItems }: App
         <S.CardHeader>{name}</S.CardHeader>
         <S.CardDate>{`지원 일자: ${formatDate(createdAt)}`}</S.CardDate>
       </S.CardDetail>
-      <div>
-        <IconButton
-          type="button"
-          outline={false}
-          onClick={togglePopOverMenu}
-        >
-          <img
-            alt="테스트"
-            src={EllipsisIcon}
+      <S.OptionButtonWrapper>
+        <div ref={optionButtonWrapperRef}>
+          <IconButton
+            type="button"
+            outline={false}
+            onClick={handleClickPopOverButton}
+          >
+            <img
+              alt="테스트"
+              src={EllipsisIcon}
+            />
+          </IconButton>
+          <PopOverMenu
+            isOpen={isPopOverOpen}
+            items={popOverMenuItems}
+            popOverPosition="3.5rem 0 0 -11.5rem"
           />
-        </IconButton>
-        <PopOverMenu
-          isOpen={isPopOverMenuOpen}
-          items={popOverMenuItems}
-        />
-      </div>
+        </div>
+      </S.OptionButtonWrapper>
     </S.CardContainer>
   );
 }
