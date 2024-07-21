cf8096c2dd49b7c8b7853f5f890618b49ca66362
Seongjin Hong <hongseongjin.to@gmail.com>
2024-07-21 16:42:20 +0900

    refactor: PopOverMenu에 노출 위치(inset) 지정 가능한 prop 추가

--- src/components/PopOverMenu/style.ts
+++ src/components/PopOverMenu/style.ts
@@ -1,18 +1,25 @@
 import styled from '@emotion/styled';
 
-const Container = styled.div<{ size: 'sm' | 'md'; isOpen: boolean }>`
+interface ContainerProps {
+  size: 'sm' | 'md';
+  isOpen: boolean;
+  popOverPosition?: string;
+}
+
+const Container = styled.div<ContainerProps>`
   width: ${({ size }) => (size === 'sm' ? '150px' : '300px')};
   position: absolute;
+  inset: ${({ popOverPosition }) => popOverPosition ?? 'inherit'};
 
   border-radius: ${({ isOpen }) => (isOpen ? '8px 8px 0px 0px' : '8px')};
   box-shadow: 0px 4px 4px ${({ theme }) => theme.baseColors.grayscale[400]};
 
   display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
+
+  z-index: 1;
 `;
 
 const List = styled.div`
-  position: relative;
-  top: 10px;
   width: 100%;
   padding: 8px;
   background-color: ${({ theme }) => theme.baseColors.grayscale[50]};
@@ -22,8 +29,6 @@ const List = styled.div`
 
   box-sizing: border-box;
   box-shadow: 0px 4px 4px ${({ theme }) => theme.baseColors.grayscale[400]};
-
-  z-index: 1;
 `;
 
 const S = {
