6282caa4a1adc1fc857018d68ad21dbdfffa91df
HyungHoKim00 <hkim1109@naver.com>
2024-07-23 16:48:44 +0900

    style: 누락된 마침표 추가

--- backend/src/main/java/com/cruru/process/exception/ProcessCountException.java
+++ backend/src/main/java/com/cruru/process/exception/ProcessCountException.java
@@ -4,7 +4,7 @@ import com.cruru.advice.badrequest.BadRequestException;
 
 public class ProcessCountException extends BadRequestException {
 
-    private static final String MESSAGE = "프로세스는 최대 %d개까지 생성 가능합니다";
+    private static final String MESSAGE = "프로세스는 최대 %d개까지 생성 가능합니다.";
 
     public ProcessCountException(int maxCount) {
         super(String.format(MESSAGE, maxCount));
