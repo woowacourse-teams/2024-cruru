9ae24ddfb0433512831ef23cc4aa9a340bee480a
HyungHoKim00 <hkim1109@naver.com>
2024-07-23 15:42:34 +0900

    refactor: 오타 및 문장 변경

--- backend/src/main/java/com/cruru/evaluation/exception/EvaluationScoreException.java
+++ backend/src/main/java/com/cruru/evaluation/exception/EvaluationScoreException.java
@@ -4,7 +4,7 @@ import com.cruru.advice.BadRequestException;
 
 public class EvaluationScoreException extends BadRequestException {
 
-    private static final String MESSAGE = "평가 점수가 %d 미만이거나 %d 초과입니다. 현재 점수: %d.";
+    private static final String MESSAGE = "평가 점수는 %d~%d 범위 안에 있어야 합니다. 현재 점수: %d.";
 
     public EvaluationScoreException(int minScore, int maxScore, int currentScore) {
         super(String.format(MESSAGE, minScore, maxScore, currentScore));
