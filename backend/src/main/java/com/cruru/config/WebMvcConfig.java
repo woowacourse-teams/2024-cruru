f5d33ae70a8893d39b7ca300a2c79193eb9ad968
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-07-23 23:27:23 +0900

    chore: cloudfront url 추가

--- backend/src/main/java/com/cruru/config/WebMvcConfig.java
+++ backend/src/main/java/com/cruru/config/WebMvcConfig.java
@@ -10,7 +10,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://localhost:3000", "www.cruru.kr", "cruru.kr")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://localhost:3000",
+                        "www.cruru.kr",
+                        "cruru.kr",
+                        "https://dbrc7l2k8z4lk.cloudfront.net"
+                )
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
