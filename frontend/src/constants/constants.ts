cfafe61323210ae74e331dbda3f2d5f5bb10b403
Seongjin Hong <hongseongjin.to@gmail.com>
2024-07-24 16:02:26 +0900

    feat: 평가 점수에 대한 타입 정의 및 상수 추가

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -1,3 +1,5 @@
+import { EvaluationScore } from '@customTypes/applicant';
+
 export const NAV_BAR_MENU = {
   applicant: '지원자 관리',
   process: '프로세스 관리',
@@ -5,3 +7,11 @@ export const NAV_BAR_MENU = {
 } as const;
 
 export const DASHBOARD_ID = 1; // TODO: 수정해야합니다.
+
+export const EVALUATION_SCORE: Record<EvaluationScore, string> = {
+  1: '별로예요',
+  2: '잘 모르겠어요',
+  3: '그저 그래요',
+  4: '좋아요',
+  5: '아주 좋아요',
+} as const;
