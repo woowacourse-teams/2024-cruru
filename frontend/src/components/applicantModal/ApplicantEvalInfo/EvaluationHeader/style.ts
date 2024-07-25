cf6b0b8576994e26a8e7cb657e526658f6228bf7
Seongjin Hong <hongseongjin.to@gmail.com>
2024-07-25 14:31:13 +0900

    feat: 지원자 평가 영역 Header 컴포넌트 구현

---
+++ frontend/src/components/applicantModal/ApplicantEvalInfo/EvaluationHeader/style.ts
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  height: 9rem;
+  padding: 0 1.6rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+
+  ${({ theme }) => theme.typography.heading[600]}
+`;
+
+const Title = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Description = styled.div`
+  ${({ theme }) => theme.typography.common.paragraph}
+`;
+
+const S = {
+  Container,
+  Title,
+  Description,
+};
+
+export default S;
