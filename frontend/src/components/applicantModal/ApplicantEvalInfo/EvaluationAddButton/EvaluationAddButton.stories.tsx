7b667f0f83b7c96c8a4365c3a0393eba8667a7d6
Seongjin Hong <hongseongjin.to@gmail.com>
2024-07-25 14:13:29 +0900

    feat: "평가 작성" 버튼 요소 구현

---
+++ frontend/src/components/applicantModal/ApplicantEvalInfo/EvaluationAddButton/EvaluationAddButton.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import EvaluationAddButton from '.';
+
+const meta = {
+  title: 'Components/ApplicantModal/ApplicantEvalInfo/EvaluationAddButton',
+  component: EvaluationAddButton,
+  parameters: {
+    layout: 'centered',
+    docs: {
+      description: {
+        component: '모달의 지원자에 대한 개별 평가 등록 버튼입니다.',
+      },
+    },
+  },
+
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <div style={{ padding: '24px', minWidth: '400px', minHeight: '40px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof EvaluationAddButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
---
+++ frontend/src/components/applicantModal/ApplicantEvalInfo/EvaluationAddButton/style.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const ButtonContainer = styled.div`
+  width: 100%;
+  height: 4.4rem;
+  margin: 2.4rem;
+
+  ${({ theme }) => theme.typography.heading[500]}
+`;
+
+const S = {
+  ButtonContainer,
+};
+
+export default S;
