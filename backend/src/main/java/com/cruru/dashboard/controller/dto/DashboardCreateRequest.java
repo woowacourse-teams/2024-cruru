06f0a5c38b27116b7e93993f18cf7ad7e8c785d0
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2024-07-31 21:00:44 +0900

    refactor(DashboardCreateRequest): 날짜 Json 파라미터 입력 포맷 설정 및 NotBlack를 NotNull로 변경

--- backend/src/main/java/com/cruru/dashboard/controller/dto/DashboardCreateRequest.java
+++ backend/src/main/java/com/cruru/dashboard/controller/dto/DashboardCreateRequest.java
@@ -1,9 +1,11 @@
 package com.cruru.dashboard.controller.dto;
 
 import com.cruru.question.controller.dto.QuestionCreateRequest;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
-
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -16,10 +18,12 @@ public record DashboardCreateRequest(
 
         List<QuestionCreateRequest> questions,
 
-        @NotBlank(message = "시작 날짜는 필수 값입니다.")
+        @NotNull(message = "시작 날짜는 필수 값입니다.")
+        @JsonFormat(shape = Shape.STRING)
         LocalDateTime startDate,
 
-        @NotBlank(message = "종료 날짜는 필수 값입니다.")
+        @NotNull(message = "종료 날짜는 필수 값입니다.")
+        @JsonFormat(shape = Shape.STRING)
         LocalDateTime dueDate
 ) {
 
