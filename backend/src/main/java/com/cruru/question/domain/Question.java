4f666c7926234540d3ff5e4bb9d9f4cbfa29278c
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-08-01 08:15:43 +0900

    fix: EnumType 추가

--- backend/src/main/java/com/cruru/question/domain/Question.java
+++ backend/src/main/java/com/cruru/question/domain/Question.java
@@ -3,6 +3,8 @@ package com.cruru.question.domain;
 import com.cruru.applyform.domain.ApplyForm;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,6 +27,7 @@ public class Question {
     @Column(name = "question_id")
     private Long id;
 
+    @Enumerated(EnumType.STRING)
     private QuestionType questionType;
 
     private String content;
