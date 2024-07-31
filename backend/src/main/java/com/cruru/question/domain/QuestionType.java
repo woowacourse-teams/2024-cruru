4eba6cc774d5d066d65ef4deba91fc4e28d06458
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2024-07-31 20:37:26 +0900

    feat(QuestionType): QuestionType 선택지 존재 여부 field 추가

--- backend/src/main/java/com/cruru/question/domain/QuestionType.java
+++ backend/src/main/java/com/cruru/question/domain/QuestionType.java
@@ -2,20 +2,26 @@ package com.cruru.question.domain;
 
 public enum QuestionType {
 
-    SHORT_ANSWER("SHORT_ANSWER"),
-    LONG_ANSWER("LONG_ANSWER"),
-    DROPDOWN("DROPDOWN"),
-    CHECK_BOX("CHECK_BOX"),
-    MULTIPLE_CHOICE("MULTIPLE_CHOICE"),
+    SHORT_ANSWER("SHORT_ANSWER", false),
+    LONG_ANSWER("LONG_ANSWER", false),
+    DROPDOWN("DROPDOWN", true),
+    CHECK_BOX("CHECK_BOX", true),
+    MULTIPLE_CHOICE("MULTIPLE_CHOICE", true),
     ;
 
     private String type;
+    private boolean hasChoice;
 
-    QuestionType(String type) {
+    QuestionType(String type, boolean hasChoice) {
         this.type = type;
+        this.hasChoice = hasChoice;
     }
 
     public static QuestionType fromString(String type) {
         return QuestionType.valueOf(type.toUpperCase());
+    }
+
+    public boolean hasChoice() {
+        return hasChoice;
     }
 }
