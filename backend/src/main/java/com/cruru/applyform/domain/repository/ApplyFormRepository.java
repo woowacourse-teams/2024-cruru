431987639f3040bd64cdc5dc201c984e199536ae
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2024-07-31 15:39:01 +0900

    feat(ApplyForm): 기초 Entity 및 Repository 구현

---
+++ backend/src/main/java/com/cruru/applyform/domain/repository/ApplyFormRepository.java
@@ -0,0 +1,8 @@
+package com.cruru.applyform.domain.repository;
+
+import com.cruru.applyform.domain.ApplyForm;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplyFormRepository extends JpaRepository<ApplyForm, Long > {
+
+}
