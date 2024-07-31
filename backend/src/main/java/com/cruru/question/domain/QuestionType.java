54605bd1f48522d9ce40fcc627339c41fbf9869b
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-08-01 08:35:07 +0900

    refactor: 불필요 타입 제거

--- backend/src/main/java/com/cruru/question/domain/QuestionType.java
+++ backend/src/main/java/com/cruru/question/domain/QuestionType.java
@@ -2,20 +2,10 @@ package com.cruru.question.domain;
 
 public enum QuestionType {
 
-    SHORT_ANSWER("SHORT_ANSWER"),
-    LONG_ANSWER("LONG_ANSWER"),
-    DROPDOWN("DROPDOWN"),
-    CHECK_BOX("CHECK_BOX"),
-    MULTIPLE_CHOICE("MULTIPLE_CHOICE"),
+    SHORT_ANSWER,
+    LONG_ANSWER,
+    DROPDOWN,
+    CHECK_BOX,
+    MULTIPLE_CHOICE,
     ;
-
-    private String type;
-
-    QuestionType(String type) {
-        this.type = type;
-    }
-
-    public static QuestionType fromString(String type) {
-        return QuestionType.valueOf(type.toUpperCase());
-    }
 }
