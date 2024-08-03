f796d51267446c50c9010e4731fe2d22718b07ec
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-08-03 10:53:05 +0900

    feat: RadioInputField 구현

---
+++ frontend/src/components/application/RadioInputField/index.tsx
@@ -0,0 +1,85 @@
+import React, { useCallback, useEffect, useRef } from 'react';
+
+import S from './style';
+import RadioInputOption from '../RadioInputOption';
+
+interface Option {
+  value: string;
+}
+
+interface Props {
+  options: Option[];
+  setOptions: React.Dispatch<React.SetStateAction<Option[]>>;
+}
+
+export default function RadioInputField({ options, setOptions }: Props) {
+  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
+
+  const handleOptionChange = (index: number, value: string) => {
+    const newOptions = [...options];
+    newOptions[index].value = value;
+    setOptions(newOptions);
+  };
+
+  const addOption = () => {
+    setOptions([...options, { value: '' }]);
+  };
+
+  const deleteOption = (index: number) => {
+    const newOptions = options.slice();
+    newOptions.splice(index, 1);
+    setOptions(newOptions);
+  };
+
+  const handleOptionBlur = (index: number) => {
+    if (index !== options.length - 1 && options[index].value.trim() === '') {
+      deleteOption(index);
+    }
+    if (index === options.length - 1 && options[index].value.trim() !== '') {
+      addOption();
+    }
+  };
+
+  const focusLastOption = useCallback(() => {
+    inputRefs.current[options.length - 1]?.focus();
+  }, [options.length]);
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, index: number) => {
+    if ((e.key === 'Tab' || e.key === 'Enter') && !e.shiftKey) {
+      e.preventDefault();
+      if (index === options.length - 1) {
+        addOption();
+      }
+      focusLastOption();
+    }
+  };
+
+  useEffect(() => {
+    focusLastOption();
+  }, [options.length, focusLastOption]);
+
+  const setInputRefCallback = (index: number) => (node: HTMLInputElement) => {
+    inputRefs.current[index] = node;
+  };
+
+  return (
+    <S.Container>
+      {options.map((option, index) => (
+        <RadioInputOption
+          // eslint-disable-next-line react/no-array-index-key
+          key={index}
+          isDisabled={false}
+          isDeleteBtn={options.length - 1 !== index}
+          onDeleteBtnClick={() => deleteOption(index)}
+          inputAttrs={{
+            value: option.value,
+            ref: setInputRefCallback(index),
+            onChange: (e) => handleOptionChange(index, e.target.value),
+            onKeyDown: (e) => handleKeyDown(e, index),
+            onBlur: () => handleOptionBlur(index),
+          }}
+        />
+      ))}
+    </S.Container>
+  );
+}
