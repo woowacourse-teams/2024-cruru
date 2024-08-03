7ae645aa25cd6d1015ceb28a1368754ad41fafc1
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2024-08-03 18:27:22 +0900

    feat(DashboardController): Dashboard 프리뷰 목록 GET 요청 API 응답 기능 구현

--- backend/src/main/java/com/cruru/dashboard/controller/DashboardController.java
+++ backend/src/main/java/com/cruru/dashboard/controller/DashboardController.java
@@ -1,11 +1,13 @@
 package com.cruru.dashboard.controller;
 
 import com.cruru.dashboard.controller.dto.DashboardCreateRequest;
+import com.cruru.dashboard.controller.dto.DashboardsOfClubResponse;
 import com.cruru.dashboard.service.facade.DashboardFacade;
 import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,9 +24,16 @@ public class DashboardController {
     @PostMapping
     public ResponseEntity<Void> create(
             @RequestParam(name = "club_id") Long clubId,
-            @RequestBody @Valid DashboardCreateRequest request) {
+            @RequestBody @Valid DashboardCreateRequest request
+    ) {
 
         long dashboardId = dashboardFacade.create(clubId, request);
         return ResponseEntity.created(URI.create("/v1/dashboards/" + dashboardId)).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<DashboardsOfClubResponse> readDashboards(@RequestParam(name = "club_id") Long clubId) {
+        DashboardsOfClubResponse dashboards = dashboardFacade.findAllDashboardsByClubId(clubId);
+        return ResponseEntity.ok().body(dashboards);
     }
 }
