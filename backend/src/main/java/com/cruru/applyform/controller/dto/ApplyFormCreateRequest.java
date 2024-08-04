38805756d959d5f515c3726d00cd03e7c26ff1dc
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-08-04 21:27:20 +0900

    feat: api 명세에 맞게 필드 변경

--- backend/src/main/java/com/cruru/applyform/controller/dto/ApplyFormCreateRequest.java
+++ backend/src/main/java/com/cruru/applyform/controller/dto/ApplyFormCreateRequest.java
@@ -12,9 +12,11 @@ public record ApplyFormCreateRequest(
         String postingContent,
 
         @NotBlank(message = "시작 날짜는 필수 값입니다.")
+        @JsonProperty("start_date")
         LocalDateTime startDate,
 
         @NotBlank(message = "종료 날짜는 필수 값입니다.")
+        @JsonProperty("end_date")
         LocalDateTime endDate
 ) {
 
