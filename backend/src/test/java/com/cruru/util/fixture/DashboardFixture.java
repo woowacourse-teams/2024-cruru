1941cbedf80ad8b4edf8a3714d98c96f05499103
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2024-08-05 19:56:16 +0900

    refactor(DashboardFixture): Dashboard 테스트용 Fixture 추가

--- backend/src/test/java/com/cruru/util/fixture/DashboardFixture.java
+++ backend/src/test/java/com/cruru/util/fixture/DashboardFixture.java
@@ -1,11 +1,16 @@
 package com.cruru.util.fixture;
 
+import com.cruru.club.domain.Club;
 import com.cruru.dashboard.domain.Dashboard;
 
 public class DashboardFixture {
 
     public static Dashboard createBackendDashboard() {
         return new Dashboard(null);
+    }
+
+    public static Dashboard createBackendDashboard(Club club) {
+        return new Dashboard(club);
     }
 
     public static Dashboard createFrontendDashboard() {
