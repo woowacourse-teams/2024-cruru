963190cb927df78181e62ca2ef6e9d9862f620c6
Seongjin Hong <hongseongjin.to@gmail.com>
2024-08-05 16:05:58 +0900

    style: font-weight별 서체 variation 적용

--- frontend/src/styles/globalStyles.ts
+++ frontend/src/styles/globalStyles.ts
@@ -1,42 +1,26 @@
 import { css } from '@emotion/react';
 
+const fontWeights = [
+  { weight: 400, name: 'Regular' },
+  { weight: 500, name: 'Medium' },
+  { weight: 600, name: 'SemiBold' },
+  { weight: 700, name: 'Bold' },
+  { weight: 800, name: 'ExtraBold' },
+  { weight: 900, name: 'Black' },
+];
+
 const globalStyles = () => css`
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
-    font-weight: 400;
-    font-style: normal;
-  }
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
-    font-weight: 500;
-    font-style: normal;
-  }
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
-    font-weight: 600;
-    font-style: normal;
-  }
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
-    font-weight: 700;
-    font-style: normal;
-  }
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
-    font-weight: 800;
-    font-style: normal;
-  }
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
-    font-weight: 900;
-    font-style: normal;
-  }
+  ${fontWeights.map(
+    ({ weight, name }) => css`
+      @font-face {
+        font-family: 'Pretendard';
+        src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-${name}.woff')
+          format('woff');
+        font-weight: ${weight};
+        font-style: normal;
+      }
+    `,
+  )}
 
   /* Reset CSS */
   * {
