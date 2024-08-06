e178f79e7b015cd4f9789ffa463155cc3a3fded5
Seongjin Hong <hongseongjin.to@gmail.com>
2024-08-06 18:19:13 +0900

    feat: 공고 카드 컴포넌트 구현

---
+++ frontend/src/components/recruitment/RecruitmentCard/index.tsx
@@ -0,0 +1,79 @@
+import { HiOutlineClock, HiOutlineShare } from 'react-icons/hi';
+import formatDate from '@utils/formatDate';
+
+import RecruitmentCardStat from '../RecruitmentCardStat';
+import IconButton from '../../common/IconButton';
+import S from './style';
+
+interface RecruitmentStats {
+  accept: number;
+  fail: number;
+  inProgress: number;
+  total: number;
+}
+
+interface RecruitmentCardProps {
+  dashboardId: number;
+  title: string;
+  postStats: RecruitmentStats;
+  endDate: string;
+  onClickTitle: (dashboardId: number) => void;
+  onClickShareButton: (dashboardId: number) => void;
+}
+
+const POST_STATS_KEY: Record<string, string> = {
+  accept: '합격',
+  fail: '불합격',
+  inProgress: '평가 대상',
+  total: '전체',
+} as const;
+
+export default function RecruitmentCard({
+  dashboardId,
+  title,
+  postStats,
+  endDate,
+  onClickTitle,
+  onClickShareButton,
+}: RecruitmentCardProps) {
+  const postStatsMap = new Map<string, number>([
+    [POST_STATS_KEY.total, postStats.total],
+    [POST_STATS_KEY.inProgress, postStats.inProgress],
+    [POST_STATS_KEY.fail, postStats.fail],
+    [POST_STATS_KEY.accept, postStats.accept],
+  ]);
+
+  return (
+    <S.CardWrapper>
+      <S.RecruitmentInfoContainer>
+        <S.RecruitmentTitle onClick={() => onClickTitle(dashboardId)}>{title}</S.RecruitmentTitle>
+        <S.EndDateContainer>
+          <HiOutlineClock size="1.6rem" />
+          <span>{formatDate(endDate)}</span>
+        </S.EndDateContainer>
+      </S.RecruitmentInfoContainer>
+
+      <S.RecruitmentResultContainer>
+        <S.PostStatsContainer>
+          {Array.from(postStatsMap).map(([label, value]) => (
+            <RecruitmentCardStat
+              key={`${dashboardId}-${label}`}
+              label={label}
+              number={value}
+              isTotalStats={label === POST_STATS_KEY.total}
+            />
+          ))}
+        </S.PostStatsContainer>
+
+        <IconButton
+          size="sm"
+          color="white"
+          outline={false}
+          onClick={() => onClickShareButton(dashboardId)}
+        >
+          <HiOutlineShare />
+        </IconButton>
+      </S.RecruitmentResultContainer>
+    </S.CardWrapper>
+  );
+}
---
+++ frontend/src/components/recruitment/RecruitmentCard/style.ts
@@ -0,0 +1,99 @@
+import styled from '@emotion/styled';
+
+const CardWrapper = styled.article`
+  width: 38rem;
+  height: 20rem;
+  border: 1px solid ${({ theme }) => theme.baseColors.grayscale[400]};
+  border-radius: 0.9rem;
+
+  display: flex;
+  flex-direction: column;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    border: 1px solid ${({ theme }) => theme.baseColors.grayscale[700]};
+    box-shadow: ${({ theme }) => `0 0.4rem 0.4rem ${theme.baseColors.grayscale[400]}`};
+  }
+`;
+
+const RecruitmentInfoContainer = styled.div`
+  height: 15rem;
+  padding: 3.2rem 1.6rem 1.6rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const RecruitmentTitle = styled.div`
+  ${({ theme }) => theme.typography.heading[600]};
+  color: ${({ theme }) => theme.baseColors.grayscale[900]};
+  height: 4.8rem;
+  word-wrap: break-word;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  cursor: pointer;
+`;
+
+const EndDateContainer = styled.div`
+  color: ${({ theme }) => theme.baseColors.grayscale[800]};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.5rem;
+
+  span {
+    ${({ theme }) => theme.typography.heading[400]};
+    color: ${({ theme }) => theme.baseColors.grayscale[700]};
+  }
+`;
+
+const RecruitmentResultContainer = styled.div`
+  border-top: 1px solid ${({ theme }) => theme.baseColors.grayscale[400]};
+  padding: 1.6rem;
+  position: relative;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  button {
+    position: absolute;
+    right: 1.6rem;
+  }
+`;
+
+const PostStatsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  span.statNumbers {
+    color: ${({ theme }) => theme.baseColors.grayscale[900]};
+    margin-left: 0.5rem;
+  }
+
+  div.totalStats {
+    padding-right: 1.6rem;
+    border-right: 1px solid ${({ theme }) => theme.baseColors.grayscale[400]};
+  }
+
+  div.stats {
+    padding-left: 1.6rem;
+  }
+`;
+
+const S = {
+  CardWrapper,
+  RecruitmentInfoContainer,
+  RecruitmentTitle,
+  EndDateContainer,
+  RecruitmentResultContainer,
+  PostStatsContainer,
+};
+
+export default S;
