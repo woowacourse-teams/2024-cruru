6efb4eb328c112def645779ac50471b9a5f605ab
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-08-07 14:00:57 +0900

    feat: dashboardCreateForm api mock 핸들러 구현

--- frontend/src/api/dashboard.ts
+++ frontend/src/api/dashboard.ts
@@ -1,1 +1,27 @@
-const dashboard;
+import { DashboardFormInfo } from '@customTypes/dashboard';
+import { DASHBOARDS } from './endPoint';
+import { convertParamsToQueryString } from './utils';
+
+const dashboardApis = {
+  create: async ({ clubId, dashboardFormInfo }: { clubId: number; dashboardFormInfo: DashboardFormInfo }) => {
+    const queryParams = {
+      clubId: String(clubId),
+    };
+
+    const response = await fetch(`${DASHBOARDS}?${convertParamsToQueryString(queryParams)}`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(dashboardFormInfo),
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    return response;
+  },
+};
+
+export default dashboardApis;
