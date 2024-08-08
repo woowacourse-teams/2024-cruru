a2f0a081ee7b977e9a32e93082dbc6ab146c8a8d
Kim Da Eun <midekuna@gmail.com>
2024-08-08 22:02:23 +0900

    feat: 지원 확인 페이지

--- frontend/src/pages/ConfirmApply/index.tsx
+++ frontend/src/pages/ConfirmApply/index.tsx
@@ -1,3 +1,45 @@
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import Button from '@components/common/Button';
+
+import { LuCheckCircle } from 'react-icons/lu';
+
+import S from './style';
+
 export default function ConfirmApply() {
-  return <div />;
+  const location = useLocation();
+  const state = location.state as { title: string };
+
+  const { postId } = useParams<{ postId: string }>();
+
+  const navigate = useNavigate();
+  const navigateToPost = () => {
+    navigate(`/post/${postId}`);
+  };
+
+  if (!state) {
+    navigateToPost();
+  }
+
+  return (
+    <S.PageLayout>
+      <S.Wrapper>
+        <LuCheckCircle />
+
+        <S.PostTitleContainer>
+          <S.Title>{`"${state.title}"`}</S.Title>
+          <S.Information>모집 공고에 지원이 완료되었습니다.</S.Information>
+        </S.PostTitleContainer>
+
+        <S.ButtonContainer>
+          <Button
+            color="primary"
+            size="fillContainer"
+            onClick={navigateToPost}
+          >
+            공고 화면으로 돌아가기
+          </Button>
+        </S.ButtonContainer>
+      </S.Wrapper>
+    </S.PageLayout>
+  );
 }
