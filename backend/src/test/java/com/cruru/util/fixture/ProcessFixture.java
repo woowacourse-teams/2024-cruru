de5320e83becab02594d5e0c317f4acd573c186e
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2024-08-08 18:39:03 +0900

    fix(ProcessFixture): 잘못 사용된 Fixture 수정

--- backend/src/test/java/com/cruru/util/fixture/ProcessFixture.java
+++ backend/src/test/java/com/cruru/util/fixture/ProcessFixture.java
@@ -8,7 +8,7 @@ public class ProcessFixture {
 
     public static List<Process> maxSizeOf(Dashboard dashboard) {
         return List.of(
-                createFinalProcess(dashboard),
+                createFirstProcess(dashboard),
                 new Process(1, "코딩 테스트", "온라인", dashboard),
                 new Process(2, "CS 테스트", "온라인", dashboard),
                 new Process(3, "1차 면접", "화상 면접", dashboard),
