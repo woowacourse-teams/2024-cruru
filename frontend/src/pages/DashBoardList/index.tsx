8abe32731f12fd9ec0b747b0b46d43703621498a
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-08-08 21:53:28 +0900

    feat: 공고 리스트 화면 구현

--- frontend/src/pages/DashBoardList/index.tsx
+++ frontend/src/pages/DashBoardList/index.tsx
@@ -1,10 +1,38 @@
+import RecruitmentCard from '@components/recruitment/RecruitmentCard';
+import { useNavigate, useParams } from 'react-router-dom';
+import useGetDashboards from '@hooks/useGetDashboards';
+import S from './style';
+
 export default function DashboardList() {
+  const { dashboardId } = useParams() as { dashboardId: string };
+  const { data } = useGetDashboards({ dashboardId });
+  const navigate = useNavigate();
+
+  const handleCardClick = (postId: number) => {
+    navigate(`/dashboard/${dashboardId}/${postId}`);
+  };
+
   return (
-    <div>
-      List
-      {/*
-      TODO: 여기에 PostList를 넣어주세요!!!!!!
-    */}
-    </div>
+    <S.Container>
+      <S.Title>{data?.clubName}</S.Title>
+      <S.CardGrid>
+        {data?.dashboards.map((dashboard) => (
+          <RecruitmentCard
+            key={dashboard.dashboardId}
+            // TODO: dashboardId -> postId로 변경
+            dashboardId={Number(dashboard.dashboardId)}
+            title={dashboard.title}
+            postStats={dashboard.stats}
+            startDate={dashboard.startDate}
+            endDate={dashboard.endDate}
+            onClick={handleCardClick}
+          />
+        ))}
+        <S.AddCard onClick={() => navigate(`/dashboard/${dashboardId}/create`)}>
+          <div>+</div>
+          <span>새 공고 추가</span>
+        </S.AddCard>
+      </S.CardGrid>
+    </S.Container>
   );
 }
