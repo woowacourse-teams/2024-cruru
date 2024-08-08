9106b30f8128184f2a52eb0d77d86c76fc7a3f41
Kim Da Eun <midekuna@gmail.com>
2024-08-08 15:12:54 +0900

    feat: useForm validation, formatter 옵션 추가

--- frontend/src/hooks/utils/useForm.ts
+++ frontend/src/hooks/utils/useForm.ts
@@ -1,13 +1,84 @@
 import { KeyedStrings } from '@customTypes/utilTypes';
-import { useState } from 'react';
+import ValidationError from '@utils/errors/ValidationError';
+import { ComponentPropsWithRef, FormEventHandler, useState } from 'react';
 
-interface UseFormProps<T> {
-  initialValues: T;
+interface RegisterOptions extends ComponentPropsWithRef<'input'> {
+  validate?: {
+    onChange?: (value: string) => void;
+    onBlur?: (value: string) => void;
+  };
+  formatter?: (value: string) => string;
 }
 
-export default function useForm<T>({ initialValues }: UseFormProps<T>) {
-  const [formValues, setFormValues] = useState<T>(initialValues);
-  const [errors, setErrors] = useState<KeyedStrings<T>>({} as KeyedStrings<T>);
+interface UseFormProps<TFieldData> {
+  initialValues: TFieldData;
+}
 
-  return { formValues };
+export default function useForm<TFieldData>({ initialValues }: UseFormProps<TFieldData>) {
+  const [formData, setFormData] = useState<TFieldData>(initialValues);
+  const [errors, setErrors] = useState<KeyedStrings<TFieldData>>({} as KeyedStrings<TFieldData>);
+
+  const validateAndSetErrors = ({
+    value,
+    validate,
+    inputName,
+  }: {
+    value: string;
+    validate: (data: string) => void;
+    inputName: string;
+  }) => {
+    try {
+      validate(value);
+      setErrors((prev) => ({ ...prev, [inputName]: '' }));
+    } catch (err) {
+      if (err instanceof ValidationError) {
+        setErrors((prev) => ({ ...prev, [err.inputName]: err.message }));
+      }
+    }
+  };
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => async (callback: () => Promise<void>) => {
+    e.preventDefault();
+
+    if (Object.values(errors).length > 0) return;
+
+    setErrors({} as KeyedStrings<TFieldData>);
+    await callback();
+  };
+
+  const register = (inputName: keyof TFieldData, options: RegisterOptions = {} as RegisterOptions) => {
+    const { validate, formatter } = options;
+    const eventHandlers = {} as Pick<RegisterOptions, 'onChange' | 'onBlur'>;
+
+    eventHandlers.onChange = (e) => {
+      options.onChange?.(e);
+
+      const { name, value } = e.target;
+      if (validate?.onChange) {
+        validateAndSetErrors({ value, validate: validate.onChange, inputName: name });
+      }
+
+      setFormData((prev) => ({ ...prev, [name]: formatter ? formatter(value) : value }));
+    };
+
+    eventHandlers.onBlur = (e) => {
+      options.onBlur?.(e);
+
+      const { name, value } = e.target;
+      if (validate?.onBlur) {
+        validateAndSetErrors({ value, validate: validate.onBlur, inputName: name });
+      }
+    };
+
+    return {
+      ...options,
+      name: inputName,
+      value: formData[inputName],
+      error: errors[inputName],
+      onChange: eventHandlers.onChange,
+      onBlur: eventHandlers.onBlur,
+    };
+  };
+
+  return { register, formData, handleSubmit, validateAndSetErrors };
 }
