d8f505bd02709a6fcfb0dfb766f87c578071130e
Kim Da Eun <midekuna@gmail.com>
2024-08-12 20:52:12 +0900

    feat: dashboard 페이지 반응형 일부 적용

--- frontend/src/components/processManagement/ProcessManageBoard/style.ts
+++ frontend/src/components/processManagement/ProcessManageBoard/style.ts
@@ -3,6 +3,8 @@ import styled from '@emotion/styled';
 const Container = styled.div`
   width: 100%;
   height: 100%;
+  overflow-x: scroll;
+  overflow-y: visible;
 
   display: flex;
   gap: 1.6rem;
