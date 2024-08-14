e77ba93290c6feaf7f1ef5fd233fc814a10040e9
Seongjin Hong <hongseongjin.to@gmail.com>
2024-08-14 14:24:00 +0900

    refactor: 수정폼 컴포넌트 구조 변경

--- frontend/src/components/applyManagement/index.tsx
+++ frontend/src/components/applyManagement/index.tsx
@@ -1,6 +1,12 @@
 import { useParams } from 'react-router-dom';
 import useApplyManagement from '@hooks/useApplyManagement';
-import Apply from '@components/dashboard/DashboardCreate/Apply';
+
+import { HiOutlinePlusCircle } from 'react-icons/hi';
+import createSimpleKey from '@utils/createSimpleKey';
+import QuestionBuilder from '@components/dashboard/DashboardCreate/Apply/QuestionBuilder';
+import { APPLY_QUESTION_HEADER, DEFAULT_QUESTIONS } from '@constants/constants';
+
+import S from './style';
 
 export default function ApplyManagement() {
   const { postId } = useParams<{ postId: string }>() as {
@@ -20,19 +26,61 @@ export default function ApplyManagement() {
   } = useApplyManagement({ postId });
 
   return (
-    <Apply
-      applyState={applyState}
-      addQuestion={addQuestion}
-      setQuestionTitle={setQuestionTitle}
-      setQuestionType={setQuestionType}
-      setQuestionOptions={setQuestionOptions}
-      setQuestionRequiredToggle={setQuestionRequiredToggle}
-      setQuestionPrev={setQuestionPrev}
-      setQuestionNext={setQuestionNext}
-      deleteQuestion={deleteQuestion}
-      prevStep={() => {}}
-      nextStep={() => alert('업데이트 되었습니다!')}
-      isModify
-    />
+    <S.Wrapper>
+      <S.Section>
+        <S.SectionTitleContainer>
+          <h2>{APPLY_QUESTION_HEADER.defaultQuestions.title}</h2>
+          <span>{APPLY_QUESTION_HEADER.defaultQuestions.description}</span>
+        </S.SectionTitleContainer>
+        <S.DefaultInputItemsContainer>
+          <S.DefaultInputItem>이름</S.DefaultInputItem>
+          <S.DefaultInputItem>이메일</S.DefaultInputItem>
+          <S.DefaultInputItem>전화번호</S.DefaultInputItem>
+        </S.DefaultInputItemsContainer>
+      </S.Section>
+
+      <S.Section>
+        <S.SectionTitleContainer>
+          <h2>{APPLY_QUESTION_HEADER.addQuestion.title}</h2>
+          <span>{APPLY_QUESTION_HEADER.addQuestion.description}</span>
+        </S.SectionTitleContainer>
+
+        {applyState.map((question, index) => {
+          if (index >= 3) {
+            return (
+              // eslint-disable-next-line react/no-array-index-key
+              <S.QuestionsContainer key={createSimpleKey(`${index}-${question.question}`)}>
+                <QuestionBuilder
+                  index={index}
+                  question={question}
+                  setQuestionTitle={setQuestionTitle}
+                  setQuestionType={setQuestionType}
+                  setQuestionOptions={setQuestionOptions}
+                  setQuestionPrev={setQuestionPrev}
+                  setQuestionNext={setQuestionNext}
+                  deleteQuestion={deleteQuestion}
+                  setQuestionRequiredToggle={setQuestionRequiredToggle}
+                />
+              </S.QuestionsContainer>
+            );
+          }
+          return null;
+        })}
+
+        {applyState.length < DEFAULT_QUESTIONS.length && (
+          <S.AddQuestionButton
+            type="button"
+            onClick={addQuestion}
+          >
+            <HiOutlinePlusCircle />
+            항목 추가
+          </S.AddQuestionButton>
+        )}
+      </S.Section>
+
+      <S.Section>
+        <button type="button">수정하기</button>
+      </S.Section>
+    </S.Wrapper>
   );
 }
