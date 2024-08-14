5d36aac02592606538cb4c6314785280af1b123f
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2024-08-14 16:28:29 +0900

    refactor(Member): Enum field DB 어노테이션 추가

--- backend/src/main/java/com/cruru/member/domain/Member.java
+++ backend/src/main/java/com/cruru/member/domain/Member.java
@@ -6,6 +6,8 @@ import com.cruru.BaseEntity;
 import com.cruru.member.exception.badrequest.MemberIllegalPhoneNumberException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -37,6 +39,7 @@ public class Member extends BaseEntity {
 
     private String phone;
 
+    @Enumerated(EnumType.STRING)
     private MemberRole role;
 
     public Member(Long id, String email, String password, String phone) {
