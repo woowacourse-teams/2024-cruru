d03e37f4bde51ffe2e3ec64705ddf4fb277a403e
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-08-15 17:36:03 +0900

    refactor(TokenProperties): token 관련 properties @ConfigurationProperties 으로 받아오도록 변경

--- backend/src/main/java/com/cruru/auth/security/JwtTokenProvider.java
+++ backend/src/main/java/com/cruru/auth/security/JwtTokenProvider.java
@@ -8,40 +8,48 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class JwtTokenProvider {
 
     private static final String ROLE = "role";
     private static final String EMAIL = "email";
 
-    @Value("${security.jwt.token.algorithm}")
-    private String algorithm;
-
-    @Value("${security.jwt.token.secret-key}")
-    private String secretKey;
-
-    @Value("${security.jwt.token.expire-length}")
-    private long validTime;
+    private final TokenProperties tokenProperties;
 
     public String create(Member member) {
         Date now = new Date();
-        Date validity = new Date(now.getTime() + validTime);
+        Date validity = new Date(now.getTime() + tokenProperties.expireLength());
 
         return Jwts.builder()
                 .claim(ROLE, member.getRole().name())
                 .claim(EMAIL, member.getEmail())
                 .setIssuedAt(now)
                 .setExpiration(validity)
-                .signWith(SignatureAlgorithm.valueOf(algorithm), secretKey.getBytes())
+                .signWith(SignatureAlgorithm.valueOf(tokenProperties.algorithm()),
+                        tokenProperties.secretKey().getBytes())
                 .compact();
     }
 
     public String extractMemberEmail(String token) {
         Claims claims = extractClaims(token);
         return claims.get(EMAIL, String.class);
+    }
+
+    private Claims extractClaims(String token) {
+        try {
+            return Jwts.parser()
+                    .setSigningKey(tokenProperties.secretKey().getBytes())
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new IllegalTokenException();
+        }
     }
 
     public String extractMemberRole(String token) {
@@ -55,18 +63,5 @@ public class JwtTokenProvider {
         Date now = new Date();
 
         return expiration.before(now);
-    }
-
-    private Claims extractClaims(String token) {
-        try {
-            return Jwts.parser()
-                    .setSigningKey(secretKey.getBytes())
-                    .parseClaimsJws(token)
-                    .getBody();
-        } catch (ExpiredJwtException e) {
-            return e.getClaims();
-        } catch (JwtException | IllegalArgumentException e) {
-            throw new IllegalTokenException();
-        }
     }
 }
