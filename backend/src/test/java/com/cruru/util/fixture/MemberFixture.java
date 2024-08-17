7c8317030f7d1269bac3f46919e02df8ad71826b
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-08-17 21:20:27 +0900

    test(MemberControllerTest): Controller test 추가

--- backend/src/test/java/com/cruru/util/fixture/MemberFixture.java
+++ backend/src/test/java/com/cruru/util/fixture/MemberFixture.java
@@ -4,6 +4,12 @@ import com.cruru.member.domain.Member;
 
 public class MemberFixture {
 
-    public static final Member DOBBY = new Member("dobby@email.com", "newPassword214!", "01011111111");
-    public static final Member RUSH = new Member("rush@email.com", "newPassword214!", "01022222222");
+    public static final Member ADMIN = new Member("admin@email.com",
+            "$2a$10$rG0JsflKdGcORjGFTURYb.npEgtvClK4.3P.EMr/o3SdekrVFxOvG", "01011111111"); // password 원문: qwer1234
+    public static final Member DOBBY = new Member("dobby@email.com",
+            "$2a$10$RKBwn5Sa7EO0lYaZqU1zSupNbPJ5/HOKcI7gNb9c2q.TiydBHUBQK",
+            "01011111111"); // password 원문: newPassword214!
+    public static final Member RUSH = new Member("rush@email.com",
+            "$2a$10$RKBwn5Sa7EO0lYaZqU1zSupNbPJ5/HOKcI7gNb9c2q.TiydBHUBQK",
+            "01022222222"); // password 원문: newPassword214!
 }
