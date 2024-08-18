1e0ef194bce629378342d59feda6749bee417ef7
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-08-18 17:15:05 +0900

    refactor(LoginFailedException): 에러 메시지 변경

--- backend/src/main/java/com/cruru/auth/exception/LoginFailedException.java
+++ backend/src/main/java/com/cruru/auth/exception/LoginFailedException.java
@@ -4,7 +4,7 @@ import com.cruru.advice.UnauthorizedException;
 
 public class LoginFailedException extends UnauthorizedException {
 
-    private static final String MESSAGE = "이메일 또는 비밀번호가 일치하지 않습니다.";
+    private static final String MESSAGE = "비밀번호가 일치하지 않습니다.";
 
     public LoginFailedException() {
         super(MESSAGE);
