f8a3a63bb87dfc2dcbcd764ddff4b140ffc1f85c
Kim Da Eun <midekuna@gmail.com>
2024-08-20 00:31:32 +0900

    chore: 이메일 주소 변경

--- frontend/src/pages/ErrorPage/index.tsx
+++ frontend/src/pages/ErrorPage/index.tsx
@@ -37,7 +37,7 @@ const ErrorMessages: Record<number | string, ErrorMessageConfig> = {
   403: {
     title: '접근이 제한되었습니다.',
     description:
-      '이 페이지에 접근할 수 있는 권한이 없습니다. 올바른 권한이 있는지 확인해 주세요.\n혹시 문제가 계속된다면, cruru@gmail.com으로 문의해 주세요.',
+      '이 페이지에 접근할 수 있는 권한이 없습니다. 올바른 권한이 있는지 확인해 주세요.\n혹시 문제가 계속된다면, cruru.dev@gmail.com으로 문의해 주세요.',
     buttons: [
       {
         label: '이전 페이지로 돌아가기',
@@ -60,7 +60,7 @@ const ErrorMessages: Record<number | string, ErrorMessageConfig> = {
   500: {
     title: '서버에 문제가 발생했습니다.',
     description:
-      '요청하신 작업을 처리하는 도중 문제가 발생했습니다. 불편을 드려 죄송합니다.\n잠시 후 다시 시도해 주세요. 문제가 계속된다면 cruru@gmail.com으로 문의해 주세요.',
+      '요청하신 작업을 처리하는 도중 문제가 발생했습니다. 불편을 드려 죄송합니다.\n잠시 후 다시 시도해 주세요. 문제가 계속된다면 cruru.dev@gmail.com으로 문의해 주세요.',
     buttons: [
       {
         label: '이전 페이지로 돌아가기',
@@ -72,7 +72,7 @@ const ErrorMessages: Record<number | string, ErrorMessageConfig> = {
   default: {
     title: '알 수 없는 오류가 발생했습니다.',
     description:
-      '요청하신 작업을 처리하는 도중 문제가 발생했습니다. 불편을 드려 죄송합니다.\n잠시 후 다시 시도해 주세요. 문제가 계속된다면 cruru@gmail.com으로 문의해 주세요.',
+      '요청하신 작업을 처리하는 도중 문제가 발생했습니다. 불편을 드려 죄송합니다.\n잠시 후 다시 시도해 주세요. 문제가 계속된다면 cruru.dev@gmail.com으로 문의해 주세요.',
     buttons: [
       {
         label: '이전 페이지로 돌아가기',
