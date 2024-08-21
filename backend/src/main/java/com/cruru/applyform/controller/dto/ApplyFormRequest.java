16fa585a806fd2c9e38e355248deaa14860eee58
cutehumanS2 <oddpinkjadeite@gmail.com>
2024-08-21 13:44:58 +0900

    fix: 시작/종료 날짜에 @NotNull 추가

--- backend/src/main/java/com/cruru/applyform/controller/dto/ApplyFormRequest.java
+++ backend/src/main/java/com/cruru/applyform/controller/dto/ApplyFormRequest.java
@@ -2,6 +2,7 @@ package com.cruru.applyform.controller.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 public record ApplyFormRequest(
@@ -10,9 +11,11 @@ public record ApplyFormRequest(
 
         String postingContent,
 
+        @NotNull(message ="시작 날짜는 필수 값입니다.")
         @JsonFormat(shape = JsonFormat.Shape.STRING)
         LocalDateTime startDate,
 
+        @NotNull(message = "종료 날짜는 필수 값입니다.")
         @JsonFormat(shape = JsonFormat.Shape.STRING)
         LocalDateTime endDate
 ) {
