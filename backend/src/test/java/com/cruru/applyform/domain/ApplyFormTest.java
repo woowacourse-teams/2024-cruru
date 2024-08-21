f3ac360f168c18ef3e2276f01114fb53f1058966
HyungHoKim00 <hkim1109@naver.com>
2024-08-21 12:59:10 +0900

    refactor: 'validateStartDateNotInPast' 로직을 ApplyForm이 아닌 ApplyFormService가 갖도록 수정

--- backend/src/test/java/com/cruru/applyform/domain/ApplyFormTest.java
+++ backend/src/test/java/com/cruru/applyform/domain/ApplyFormTest.java
@@ -3,7 +3,6 @@ package com.cruru.applyform.domain;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.cruru.applyform.exception.badrequest.StartDateAfterEndDateException;
-import com.cruru.applyform.exception.badrequest.StartDatePastException;
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,19 +22,5 @@ class ApplyFormTest {
         // when&then
         assertThatThrownBy(() -> new ApplyForm(title, description, startDate, endDate, null))
                 .isInstanceOf(StartDateAfterEndDateException.class);
-    }
-
-    @DisplayName("시작 날짜가 현재 날짜보다 이전일 경우 예외가 발생한다.")
-    @Test
-    void startDateInPast() {
-        // given
-        String title = "title";
-        String description = "description";
-        LocalDateTime startDate = LocalDateTime.now().minusDays(1);
-        LocalDateTime endDate = LocalDateTime.now().plusDays(1);
-
-        // when&then
-        assertThatThrownBy(() -> new ApplyForm(title, description, startDate, endDate, null))
-                .isInstanceOf(StartDatePastException.class);
     }
 }
