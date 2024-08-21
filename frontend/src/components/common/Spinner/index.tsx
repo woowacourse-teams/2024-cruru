7f16ffd10842cfaa6ae20054f5c1d3ff051cfaeb
Kim Da Eun <midekuna@gmail.com>
2024-08-21 13:59:56 +0900

    feat: Spinner에 색상 옵션 추가

--- frontend/src/components/common/Spinner/index.tsx
+++ frontend/src/components/common/Spinner/index.tsx
@@ -1,9 +1,12 @@
 import type { StyleProps } from './style';
 import S from './style';
 
-export default function Spinner({ width }: StyleProps) {
+export default function Spinner({ width, color = 'white' }: StyleProps) {
   return (
-    <S.Spinner width={width}>
+    <S.Spinner
+      width={width}
+      color={color}
+    >
       <S.Bounce className="bounce1" />
       <S.Bounce className="bounce2" />
       <S.Bounce />
