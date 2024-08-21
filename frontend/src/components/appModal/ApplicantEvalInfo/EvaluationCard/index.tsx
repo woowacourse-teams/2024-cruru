ecbeafb287df3b089481188b8726a09f9a668650
Seongjin Hong <hongseongjin.to@gmail.com>
2024-08-21 20:49:04 +0900

    fix: 평가 작성자 이름, placeholder 삭제

--- frontend/src/components/appModal/ApplicantEvalInfo/EvaluationCard/index.tsx
+++ frontend/src/components/appModal/ApplicantEvalInfo/EvaluationCard/index.tsx
@@ -1,8 +1,9 @@
 import { EvaluationResult } from '@customTypes/applicant';
 import formatDate from '@utils/formatDate';
 
-import S from './style';
+import { HiOutlineClock } from 'react-icons/hi';
 import { EVALUATION_SCORE } from '../constants';
+import S from './style';
 
 interface EvaluationCardProps {
   evaluationResult: EvaluationResult;
@@ -10,22 +11,22 @@ interface EvaluationCardProps {
 
 export default function EvaluationCard({ evaluationResult }: EvaluationCardProps) {
   /**
-   * 평가자 이름 삽입 기능 추가시 수정 예정입니다. (24/8/16)
+   * 추후 평가자 이름 삽입 기능이 추가되면 아래 내용을 적용할 예정입니다.
+   * - by 아르, 24.08.21
    */
-  const evaluatorName = evaluationResult.evaluatorName || '평가자 이름';
+  // const evaluatorName = evaluationResult.evaluatorName || '평가자 이름';
   const createdDate = evaluationResult.createdDate ? formatDate(evaluationResult.createdDate) : '날짜 정보 없음';
 
   return (
     <S.CardContainer>
-      <S.EvaluatorDetailContainer>
-        <S.EvaluatorImagePlaceholder />
-        <S.EvaluatorDetail>
-          <S.EvaluatorName>{evaluatorName}</S.EvaluatorName>
-          <S.EvaluatedDate>{createdDate}</S.EvaluatedDate>
-        </S.EvaluatorDetail>
-      </S.EvaluatorDetailContainer>
       <S.ResultFlag $score={evaluationResult.score}>{EVALUATION_SCORE[evaluationResult.score]}</S.ResultFlag>
       <S.ResultComment>{evaluationResult.content}</S.ResultComment>
+      <S.EvaluatorDetailContainer>
+        <S.EvaluatedDate>
+          <HiOutlineClock size="1.2rem" />
+          {createdDate}
+        </S.EvaluatedDate>
+      </S.EvaluatorDetailContainer>
     </S.CardContainer>
   );
 }
