0ec46488e783440b13635fe45793e697b3f51538
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-09-08 16:49:26 +0900

    docs: auth API 문서화

--- backend/src/test/java/com/cruru/auth/controller/AuthControllerTest.java
+++ backend/src/test/java/com/cruru/auth/controller/AuthControllerTest.java
@@ -1,5 +1,13 @@
 package com.cruru.auth.controller;
 
+import static org.springframework.restdocs.cookies.CookieDocumentation.cookieWithName;
+import static org.springframework.restdocs.cookies.CookieDocumentation.requestCookies;
+import static org.springframework.restdocs.cookies.CookieDocumentation.responseCookies;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.restassured.RestAssuredRestDocumentation.document;
+
 import com.cruru.auth.controller.dto.LoginRequest;
 import com.cruru.club.domain.repository.ClubRepository;
 import com.cruru.member.domain.Member;
@@ -40,23 +48,39 @@ class AuthControllerTest extends ControllerTest {
         LoginRequest request = new LoginRequest(member.getEmail(), "qwer1234");
 
         // when&&then
-        RestAssured.given().log().all()
+        RestAssured.given(spec).log().all()
                 .contentType(ContentType.JSON)
                 .body(request)
+                .filter(document("auth/login",
+                        requestFields(
+                                fieldWithPath("email").description("사용자 이메일"),
+                                fieldWithPath("password").description("사용자 패스워드")
+                        ),
+                        responseFields(
+                                fieldWithPath("clubId").description("동아리의 id")
+                        ),
+                        responseCookies(cookieWithName("token").description("사용자 토큰"))
+                ))
                 .when().post("/v1/auth/login")
                 .then().log().all().statusCode(200);
     }
 
     @DisplayName("잘못된 password로 로그인 시도시 401을 응답한다.")
     @Test
-    void login_unverfiedPassword() {
+    void login_unverifiedPassword() {
         // given
         LoginRequest request = new LoginRequest(member.getEmail(), "wrongPassword");
 
         // when&&then
-        RestAssured.given().log().all()
+        RestAssured.given(spec).log().all()
                 .contentType(ContentType.JSON)
                 .body(request)
+                .filter(document("auth/login-fail/invalid-password",
+                        requestFields(
+                                fieldWithPath("email").description("사용자 이메일"),
+                                fieldWithPath("password").description("잘못된 패스워드")
+                        )
+                ))
                 .when().post("/v1/auth/login")
                 .then().log().all().statusCode(401);
     }
@@ -64,10 +88,13 @@ class AuthControllerTest extends ControllerTest {
     @DisplayName("로그아웃을 성공하면 204를 반환한다.")
     @Test
     void logout() {
-        // when&&then
-        RestAssured.given().log().all()
+        // given&when&&then
+        RestAssured.given(spec).log().all()
                 .cookie("token", token)
                 .contentType(ContentType.JSON)
+                .filter(document("auth/logout",
+                        requestCookies(cookieWithName("token").description("사용자 토큰"))
+                ))
                 .when().post("/v1/auth/logout")
                 .then().log().all().statusCode(204);
     }
