e96600a19ed8173e8964a94752117f5657c516cc
cutehumanS2 <oddpinkjadeite@gmail.com>
2024-09-11 13:30:09 +0900

    docs: API 문서 작성
    Co-Authored-By: 김형호 <140397285+HyungHoKim00@users.noreply.github.com>

--- backend/src/test/java/com/cruru/notice/controller/EmailControllerTest.java
+++ backend/src/test/java/com/cruru/notice/controller/EmailControllerTest.java
@@ -1,5 +1,11 @@
 package com.cruru.notice.controller;
 
+import static org.springframework.restdocs.cookies.CookieDocumentation.cookieWithName;
+import static org.springframework.restdocs.cookies.CookieDocumentation.requestCookies;
+import static org.springframework.restdocs.request.RequestDocumentation.partWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParts;
+import static org.springframework.restdocs.restassured.RestAssuredRestDocumentation.document;
+
 import com.cruru.applicant.domain.Applicant;
 import com.cruru.applicant.domain.repository.ApplicantRepository;
 import com.cruru.util.ControllerTest;
@@ -7,6 +13,7 @@ import com.cruru.util.fixture.ApplicantFixture;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import jakarta.mail.internet.MimeMessage;
+import java.io.File;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -35,15 +42,27 @@ class EmailControllerTest extends ControllerTest {
         applicantRepository.save(applicant);
         String subject = "[우아한테크코스] 7기 최종 심사 결과 안내";
         String text = "우아한테크코스 합격을 진심으로 축하합니다!";
+        File file = new File(getClass().getClassLoader().getResource("static/email_test.txt").getFile());
 
         // when&then
-        RestAssured.given().log().all()
+        RestAssured.given(spec).log().all()
                 .cookie("token", token)
                 .multiPart("clubId", defaultClub.getId())
                 .multiPart("to", applicant.getEmail())
                 .multiPart("subject", subject)
                 .multiPart("text", text)
+                .multiPart("files", file)
                 .contentType(ContentType.MULTIPART)
+                .filter(document("email/send",
+                        requestCookies(cookieWithName("token").description("사용자 토큰")),
+                        requestParts(
+                                partWithName("clubId").description("발송 동아리 id"),
+                                partWithName("to").description("수신자 이메일 목록"),
+                                partWithName("subject").description("이메일 제목"),
+                                partWithName("text").description("이메일 본문"),
+                                partWithName("files").description("이메일 첨부 파일")
+                        )
+                ))
                 .when().post("/v1/emails")
                 .then().log().all().statusCode(200);
     }
@@ -60,15 +79,27 @@ class EmailControllerTest extends ControllerTest {
         applicantRepository.save(applicant);
         String subject = "[우아한테크코스] 7기 최종 심사 결과 안내";
         String text = "우아한테크코스 합격을 진심으로 축하합니다!";
+        File file = new File(getClass().getClassLoader().getResource("static/email_test.txt").getFile());
 
         // when&then
-        RestAssured.given().log().all()
+        RestAssured.given(spec).log().all()
                 .cookie("token", token)
                 .multiPart("clubId", defaultClub.getId())
                 .multiPart("to", "notEmail")
                 .multiPart("subject", subject)
                 .multiPart("text", text)
+                .multiPart("files", file)
                 .contentType(ContentType.MULTIPART)
+                .filter(document("email/send-fail/invalid-email",
+                        requestCookies(cookieWithName("token").description("사용자 토큰")),
+                        requestParts(
+                                partWithName("clubId").description("발송 동아리 id"),
+                                partWithName("to").description("적절하지 않은 수신자 이메일이 포함된 목록"),
+                                partWithName("subject").description("이메일 제목"),
+                                partWithName("text").description("이메일 본문"),
+                                partWithName("files").description("이메일 첨부 파일")
+                        )
+                ))
                 .when().post("/v1/emails")
                 .then().log().all().statusCode(400);
     }
@@ -88,15 +119,27 @@ class EmailControllerTest extends ControllerTest {
         String email = applicant.getEmail();
         String subject = "[우아한테크코스] 7기 최종 심사 결과 안내";
         String text = "우아한테크코스 합격을 진심으로 축하합니다!";
+        File file = new File(getClass().getClassLoader().getResource("static/email_test.txt").getFile());
 
         // when&then
-        RestAssured.given().log().all()
+        RestAssured.given(spec).log().all()
                 .cookie("token", token)
                 .multiPart("clubId", invalidId)
                 .multiPart("to", email)
                 .multiPart("subject", subject)
                 .multiPart("text", text)
+                .multiPart("files", file)
                 .contentType(ContentType.MULTIPART)
+                .filter(document("email/send-fail/club-not-found",
+                        requestCookies(cookieWithName("token").description("사용자 토큰")),
+                        requestParts(
+                                partWithName("clubId").description("존재하지 않는 발송 동아리 id"),
+                                partWithName("to").description("수신자 이메일 목록"),
+                                partWithName("subject").description("이메일 제목"),
+                                partWithName("text").description("이메일 본문"),
+                                partWithName("files").description("이메일 첨부 파일")
+                        )
+                ))
                 .when().post("/v1/emails")
                 .then().log().all().statusCode(404);
     }
