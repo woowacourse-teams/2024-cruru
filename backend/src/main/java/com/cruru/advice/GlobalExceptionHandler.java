3bd837d490ef607da27810d10b92319e1eaa03c2
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2024-09-11 17:44:00 +0900

    refactor(GlobalExceptionHandler): 전역 예외 처리 방식 수정 및 로깅 적용

--- backend/src/main/java/com/cruru/advice/GlobalExceptionHandler.java
+++ backend/src/main/java/com/cruru/advice/GlobalExceptionHandler.java
@@ -1,50 +1,32 @@
 package com.cruru.advice;
 
 import com.cruru.advice.badrequest.BadRequestException;
+import com.cruru.global.util.ExceptionLogger;
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.context.request.WebRequest;
 
-@Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
-    private void logException(HttpServletRequest request, Exception exception, HttpStatus status) {
-        // 스택 트레이스를 통해 예외가 발생한 클래스와 메서드 정보를 추출
-        StackTraceElement[] stackTrace = exception.getStackTrace();
-        StackTraceElement origin = stackTrace[0];
+    private final UncatchedExceptionHandler handler;
 
-        String className = origin.getClassName();
-        String methodName = origin.getMethodName();
+    @ExceptionHandler
+    public ResponseEntity<ProblemDetail> handleBadRequestException(BadRequestException e) {
+        HttpServletRequest request = getCurrentHttpRequest();
+        ExceptionLogger.info(request, e, HttpStatus.BAD_REQUEST);
 
-        String httpRequestMethod = request.getMethod();
-        String requestUri = request.getRequestURI();
-        String statusCode = String.valueOf(status.value());
-        String exceptionName = exception.getClass().getSimpleName();
-        String exceptionMessage = exception.getMessage();
-
-        String logMessage = String.format(
-                "\"httpRequestMethod\"=\"%s\", \"requestUri\"=\"%s\", \"statusCode\"=\"%s\", \"source-class\"=\"%s\", \"source-method\"=\"%s\", \"exception_class\"=\"%s\", \"exception_message\"=\"%s\", \"labels\"={ \"module\"=\"user-service\", \"environment\"=\"prod\" }",
-                httpRequestMethod,
-                requestUri,
-                statusCode,
-                className,
-                methodName,
-                exceptionName,
-                exceptionMessage
-        );
-        log.warn(logMessage, exception);
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
+        return ResponseEntity.of(problemDetail).build();
     }
 
     private HttpServletRequest getCurrentHttpRequest() {
@@ -52,18 +34,9 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler
-    public ResponseEntity<ProblemDetail> handleBadRequestException(BadRequestException e) {
-        HttpServletRequest request = getCurrentHttpRequest();
-        logException(request, e, HttpStatus.BAD_REQUEST);
-
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
-        return ResponseEntity.of(problemDetail).build();
-    }
-
-    @ExceptionHandler
     public ResponseEntity<ProblemDetail> handleUnauthorizedException(UnauthorizedException e) {
         HttpServletRequest request = getCurrentHttpRequest();
-        logException(request, e, HttpStatus.UNAUTHORIZED);
+        ExceptionLogger.info(request, e, HttpStatus.UNAUTHORIZED);
 
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, e.getMessage());
         return ResponseEntity.of(problemDetail).build();
@@ -72,7 +45,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<ProblemDetail> handleForbiddenException(ForbiddenException e) {
         HttpServletRequest request = getCurrentHttpRequest();
-        logException(request, e, HttpStatus.FORBIDDEN);
+        ExceptionLogger.info(request, e, HttpStatus.FORBIDDEN);
 
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, e.getMessage());
         return ResponseEntity.of(problemDetail).build();
@@ -81,29 +54,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<ProblemDetail> handleNotFoundException(NotFoundException e) {
         HttpServletRequest request = getCurrentHttpRequest();
-        logException(request, e, HttpStatus.NOT_FOUND);
+        ExceptionLogger.info(request, e, HttpStatus.NOT_FOUND);
 
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
         return ResponseEntity.of(problemDetail).build();
     }
 
     @ExceptionHandler
-    public ResponseEntity<ProblemDetail> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        HttpServletRequest request = getCurrentHttpRequest();
-        logException(request, e, HttpStatus.BAD_REQUEST);
-
-        Map<String, String> validation = new HashMap<>();
-        for (FieldError fieldError : e.getFieldErrors()) {
-            validation.put(fieldError.getField(), fieldError.getDefaultMessage());
-        }
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, validation.toString());
-        return ResponseEntity.of(problemDetail).build();
-    }
-
-    @ExceptionHandler
     public ResponseEntity<ProblemDetail> handleConflictException(ConflictException e) {
         HttpServletRequest request = getCurrentHttpRequest();
-        logException(request, e, HttpStatus.CONFLICT);
+        ExceptionLogger.info(request, e, HttpStatus.CONFLICT);
 
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, e.getMessage());
         return ResponseEntity.of(problemDetail).build();
@@ -112,21 +72,33 @@ public class GlobalExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<ProblemDetail> handleInternalServerException(InternalServerException e) {
         HttpServletRequest request = getCurrentHttpRequest();
-        logException(request, e, HttpStatus.INTERNAL_SERVER_ERROR);
+        ExceptionLogger.info(request, e, HttpStatus.INTERNAL_SERVER_ERROR);
 
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         return ResponseEntity.of(problemDetail).build();
     }
 
-    @ExceptionHandler
-    public ResponseEntity<ProblemDetail> handleUnexpectedException(Exception e) {
-        HttpServletRequest request = getCurrentHttpRequest();
-        logException(request, e, HttpStatus.INTERNAL_SERVER_ERROR);
-
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
-                HttpStatus.INTERNAL_SERVER_ERROR,
-                "예기치 못한 오류가 발생하였습니다."
-        );
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleUnexpectedException(Exception e, WebRequest request) {
+        HttpServletRequest httpRequest = getCurrentHttpRequest();
+        ProblemDetail problemDetail = handleException(e, request);
+        HttpStatus statusCode = HttpStatus.valueOf(problemDetail.getStatus());
+        if (statusCode.is5xxServerError()) {
+            ExceptionLogger.error(httpRequest, e, statusCode);
+        } else {
+            ExceptionLogger.warn(httpRequest, e, statusCode);
+        }
         return ResponseEntity.of(problemDetail).build();
+    }
+
+    private ProblemDetail handleException(Exception e, WebRequest request) {
+        try {
+            return (ProblemDetail) Objects.requireNonNull(handler.handleException(e, request)).getBody();
+        } catch (Exception ex) {
+            return ProblemDetail.forStatusAndDetail(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    "예기치 못한 오류가 발생하였습니다."
+            );
+        }
     }
 }
---
+++ backend/src/main/java/com/cruru/advice/UncatchedExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.cruru.advice;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Component
+public class UncatchedExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException e,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+        Map<String, Object> validation = new HashMap<>();
+        for (FieldError fieldError : e.getFieldErrors()) {
+            validation.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, validation.toString());
+        ProblemDetail defaultDetail = e.getBody();
+        defaultDetail.setProperties(validation);
+        return ResponseEntity.of(problemDetail).build();
+    }
+}
