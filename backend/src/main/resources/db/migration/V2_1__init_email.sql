9bd3a620336f4e1f96d07cbeca0ca60562e8c87c
cutehumanS2 <oddpinkjadeite@gmail.com>
2024-09-11 13:40:21 +0900

    chore: 테이블명 변경 및 불필요한 필드 제거
    Co-Authored-By: 김형호 <140397285+HyungHoKim00@users.noreply.github.com>

--- backend/src/main/resources/db/migration/V2_1__init_email.sql
+++ backend/src/main/resources/db/migration/V2_1__init_email.sql
@@ -1,13 +1,12 @@
-CREATE TABLE notice
+CREATE TABLE email
 (
-    notice_id    BIGINT NOT NULL AUTO_INCREMENT,
+    email_id     BIGINT NOT NULL AUTO_INCREMENT,
     applicant_id BIGINT,
     club_id      BIGINT,
     created_date DATETIME(6),
     updated_date DATETIME(6),
     subject      VARCHAR(1023),
     text         TEXT,
-    type         VARCHAR(255),
     PRIMARY KEY (notice_id)
 ) ENGINE=InnoDB
   DEFAULT CHARSET = utf8mb4
