a508d3bb7af1e5f9569def2801bd1993dae3b62f
Kim Da Eun <midekuna@gmail.com>
2024-09-12 13:18:46 +0900

    feat: 메인 섹션 마크업 및 스타일링

--- frontend/src/pages/Landing/index.tsx
+++ frontend/src/pages/Landing/index.tsx
@@ -1,22 +1,56 @@
-import { useNavigate } from 'react-router-dom';
-import Logo from '@assets/images/logo.svg';
-import { useEffect } from 'react';
+import Button from '@components/common/Button';
+
 import { routes } from '@router/path';
+import { useNavigate } from 'react-router-dom';
+
+import DashboardPng from '@assets/images/dashboard.png';
+import { HiChevronDown } from 'react-icons/hi2';
+
 import S from './style';
 
 export default function Landing() {
   const navigate = useNavigate();
-
-  useEffect(() => {
-    setTimeout(() => navigate(routes.signIn()), 2000);
-  }, [navigate]);
-
   return (
     <S.Container>
-      <S.Logo
-        src={Logo}
-        alt="크루루 로고"
-      />
+      <S.MainSection>
+        <S.Catchphrase>
+          복잡했던 리크루팅,
+          <br />
+          하루만에
+          <S.HightLight> 크루루.</S.HightLight>
+        </S.Catchphrase>
+        <S.Supporting>
+          번거로운 엑셀 작업은 이제 그만!
+          <br />
+          리크루팅의 모든 단계를 크루루와 함께 해결하세요
+        </S.Supporting>
+
+        <S.MainImg
+          src={DashboardPng}
+          alt="지원자 현황을 한 눈에 확인할 수 있는 대시보드 페이지"
+        />
+
+        <S.CtaButtons>
+          <Button
+            size="md"
+            onClick={() => navigate(routes.signIn())}
+            color="white"
+          >
+            로그인하기
+          </Button>
+          <Button
+            size="md"
+            onClick={() => navigate(routes.signUp())}
+            color="primary"
+          >
+            크루루 새로 시작하기
+          </Button>
+        </S.CtaButtons>
+
+        <S.ScrollDownArea>
+          <HiChevronDown />
+        </S.ScrollDownArea>
+      </S.MainSection>
     </S.Container>
   );
 }
--- frontend/src/pages/Landing/style.ts
+++ frontend/src/pages/Landing/style.ts
@@ -1,14 +1,4 @@
-import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
-
-const fadeIn = keyframes`
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-`;
 
 const Container = styled.div`
   width: 100vw;
@@ -18,14 +8,119 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
 `;
-const Logo = styled.img`
-  height: 6rem;
-  animation: ${fadeIn} 1.5s ease-out;
+
+// Main Section
+const MainSection = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  padding-top: 10rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 3.6rem;
+
+  text-align: center;
+
+  background: linear-gradient(90deg, #f5f5ff 0%, ${({ theme }) => theme.baseColors.grayscale[50]} 49%);
+
+  overflow: hidden;
+`;
+
+const Catchphrase = styled.h1`
+  font-family: 'Noto Sans KR', sans-serif;
+  font-weight: 900;
+  font-size: 6.4rem;
+  letter-spacing: -1px;
+`;
+
+const HightLight = styled.span`
+  background: linear-gradient(90deg, #aa2298 0%, #841b76 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
+const Supporting = styled.p`
+  font-family: 'Pretendard', sans-serif;
+  font-weight: 500;
+  font-size: 2.4rem;
+  line-height: 1.4;
+
+  color: ${({ theme }) => theme.baseColors.grayscale[700]};
+`;
+
+const CtaButtons = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.5rem;
+
+  & > button {
+    box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.25);
+    -webkit-box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.25);
+    -moz-box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.25);
+  }
+`;
+
+const MainImg = styled.img`
+  width: 93.8rem;
+  position: absolute;
+  bottom: -18rem;
+  z-index: 0;
+`;
+
+const ScrollDownArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 10vh;
+
+  position: absolute;
+  bottom: 0;
+
+  background: rgb(243, 217, 238);
+  background: -moz-linear-gradient(
+    0deg,
+    rgba(243, 217, 238, 1) 0%,
+    rgba(255, 255, 255, 0) 50%,
+    rgba(255, 255, 255, 0) 100%
+  );
+  background: -webkit-linear-gradient(
+    0deg,
+    rgba(243, 217, 238, 1) 0%,
+    rgba(255, 255, 255, 0) 50%,
+    rgba(255, 255, 255, 0) 100%
+  );
+  background: linear-gradient(0deg, rgba(243, 217, 238, 1) 0%, rgba(255, 255, 255, 0) 50%, rgba(255, 255, 255, 0) 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#f3d9ee",endColorstr="#ffffff",GradientType=1);
+
+  & > svg {
+    display: block;
+    font-size: 4rem;
+    color: ${({ theme }) => theme.baseColors.purplescale[500]};
+  }
+`;
+
+// Pain Point Section
+const PainPointSection = styled.div`
+  width: 100%;
+  height: 100vh;
 `;
 
 const S = {
   Container,
-  Logo,
+  MainSection,
+  Catchphrase,
+  HightLight,
+  Supporting,
+  CtaButtons,
+  MainImg,
+  ScrollDownArea,
+
+  PainPointSection,
 };
 
 export default S;
