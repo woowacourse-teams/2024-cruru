b80309526430d37cf543de7c8aff921545265a4a
cutehumanS2 <oddpinkjadeite@gmail.com>
2024-09-16 10:23:34 +0900

    chore: flyway 이메일 본문 필드명 변경 및 제약 조건 추가
    Co-Authored-By: 김형호 <140397285+HyungHoKim00@users.noreply.github.com>

--- backend/src/main/resources/db/migration/V2_1__init_email.sql
+++ backend/src/main/resources/db/migration/V2_1__init_email.sql
@@ -6,8 +6,18 @@ CREATE TABLE email
     created_date DATETIME(6),
     updated_date DATETIME(6),
     subject      VARCHAR(1023),
-    text         TEXT,
+    content         TEXT,
     PRIMARY KEY (notice_id)
 ) ENGINE=InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
+
+ALTER TABLE email
+    ADD CONSTRAINT fk_email_to_club
+        FOREIGN KEY (club_id)
+            REFERENCES club (club_id)
+
+ALTER TABLE email
+    ADD CONSTRAINT fk_email_to_applicant
+        FOREIGN KEY (applicant_id)
+            REFERENCES applicant (applicant_id)
