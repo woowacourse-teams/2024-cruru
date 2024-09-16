1eb7727d52ac0ba25181bf822c689cdc0eb367e9
cutehumanS2 <oddpinkjadeite@gmail.com>
2024-09-16 11:00:51 +0900

    feat: 이메일 제목 및 본문 DTO단에서 검증 추가
    Co-Authored-By: 김형호 <140397285+HyungHoKim00@users.noreply.github.com>

--- backend/src/main/java/com/cruru/email/controller/dto/EmailRequest.java
+++ backend/src/main/java/com/cruru/email/controller/dto/EmailRequest.java
@@ -15,8 +15,10 @@ public record EmailRequest(
         @Valid
         List<@NotNull(message = "수신자는 필수 값입니다.") Long> applicantIds,
 
+        @NotNull(message = "이메일 제목은 필수 값입니다.")
         String subject,
 
+        @NotNull(message = "이메일 본문은 필수 값입니다.")
         String content,
 
         List<MultipartFile> files
