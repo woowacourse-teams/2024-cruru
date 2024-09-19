3bbd32e806966d1e9490e64d0234d8556f853385
cutehumanS2 <oddpinkjadeite@gmail.com>
2024-09-19 17:47:44 +0900

    chore: isSucceed 필드 추가 및 제약조건 분리

--- backend/src/main/resources/db/migration/V2_1__init_email.sql
+++ backend/src/main/resources/db/migration/V2_1__init_email.sql
@@ -6,18 +6,9 @@ CREATE TABLE email
     created_date DATETIME(6),
     updated_date DATETIME(6),
     subject      VARCHAR(1023),
-    content         TEXT,
+    content      TEXT,
+    is_succeed   BOOLEAN,
     PRIMARY KEY (notice_id)
 ) ENGINE=InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
-
-ALTER TABLE email
-    ADD CONSTRAINT fk_email_to_club
-        FOREIGN KEY (club_id)
-            REFERENCES club (club_id)
-
-ALTER TABLE email
-    ADD CONSTRAINT fk_email_to_applicant
-        FOREIGN KEY (applicant_id)
-            REFERENCES applicant (applicant_id)
