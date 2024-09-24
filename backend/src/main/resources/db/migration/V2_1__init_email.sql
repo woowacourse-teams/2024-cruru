7ec4a890f7c9478f363e6b20abc33a296d591336
최가희 <60508828+cutehumanS2@users.noreply.github.com>
2024-09-24 10:53:23 +0900

    chore: pk 컬럼명 변경

--- backend/src/main/resources/db/migration/V2_1__init_email.sql
+++ backend/src/main/resources/db/migration/V2_1__init_email.sql
@@ -8,7 +8,7 @@ CREATE TABLE email
     subject      VARCHAR(1023),
     content      TEXT,
     is_succeed   BOOLEAN,
-    PRIMARY KEY (notice_id)
+    PRIMARY KEY (email_id)
 ) ENGINE=InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
