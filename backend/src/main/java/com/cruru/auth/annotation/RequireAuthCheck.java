2a8206a9d6c4f48279b1f58092524c027081d54d
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-09-24 10:19:25 +0900

    refactor(RequireAuthCheck): 불필요 공백 제거

--- backend/src/main/java/com/cruru/auth/annotation/RequireAuthCheck.java
+++ backend/src/main/java/com/cruru/auth/annotation/RequireAuthCheck.java
@@ -9,6 +9,8 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RequireAuthCheck {
+
     String targetId();  // 권한을 확인할 대상 리소스 (예: "clubId", "dashboardId")
-    Class<? extends SecureResource> targetDomain() ;
+
+    Class<? extends SecureResource> targetDomain();
 }
