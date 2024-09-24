21c99f6b3e21cdbb63c83613e4e730d6a3c8a205
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-09-24 21:51:35 +0900

    feat(DataSourceConfig): DataSourceConfig 운영환경에서만 동작하도록 profile 지정

--- backend/src/main/java/com/cruru/config/DataSourceRouter.java
+++ backend/src/main/java/com/cruru/config/DataSourceRouter.java
@@ -1,8 +1,10 @@
 package com.cruru.config;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+@Profile("prod")
 public class DataSourceRouter extends AbstractRoutingDataSource {
 
     public static final String READ_DATASOURCE_KEY = "read";
