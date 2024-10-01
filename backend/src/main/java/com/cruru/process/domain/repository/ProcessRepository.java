0b02c50ed4116ea56d27f1aa359d46edf7ff774d
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-10-01 11:23:09 +0900

    feat(ProcessService): Process 조회시 Member fetch join 하는 메서드 추가

--- backend/src/main/java/com/cruru/process/domain/repository/ProcessRepository.java
+++ backend/src/main/java/com/cruru/process/domain/repository/ProcessRepository.java
@@ -3,7 +3,10 @@ package com.cruru.process.domain.repository;
 import com.cruru.dashboard.domain.Dashboard;
 import com.cruru.process.domain.Process;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProcessRepository extends JpaRepository<Process, Long> {
 
@@ -12,4 +15,13 @@ public interface ProcessRepository extends JpaRepository<Process, Long> {
     int countByDashboard(Dashboard dashboard);
 
     List<Process> findAllByDashboard(Dashboard dashboard);
+
+    @Query("""
+           SELECT p FROM Process p 
+           JOIN FETCH p.dashboard d
+           JOIN FETCH d.club c
+           JOIN FETCH c.member
+           WHERE p.id = :id
+           """)
+    Optional<Process> findByIdFetchingMember(@Param("id") long id);
 }
