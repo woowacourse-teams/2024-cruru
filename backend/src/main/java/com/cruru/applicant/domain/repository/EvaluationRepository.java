e69154230d249746065007d409a07d004803cbd0
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-10-02 16:42:13 +0900

    refactor: Query import 재배치

--- backend/src/main/java/com/cruru/applicant/domain/repository/EvaluationRepository.java
+++ backend/src/main/java/com/cruru/applicant/domain/repository/EvaluationRepository.java
@@ -7,8 +7,8 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
