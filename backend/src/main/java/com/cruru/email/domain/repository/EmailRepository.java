69deb7535803eeb1630ce75952daf85a728a28ba
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-10-02 15:01:38 +0900

    refactor: 불필요 import 제거

--- backend/src/main/java/com/cruru/email/domain/repository/EmailRepository.java
+++ backend/src/main/java/com/cruru/email/domain/repository/EmailRepository.java
@@ -1,10 +1,7 @@
 package com.cruru.email.domain.repository;
 
 import com.cruru.email.domain.Email;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface EmailRepository extends JpaRepository<Email, Long> {
 
