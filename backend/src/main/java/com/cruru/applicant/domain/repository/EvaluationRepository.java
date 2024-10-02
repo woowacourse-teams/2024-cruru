c7d1fd4d0d326b41c0f683f54fbe8fe0b494350f
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-10-02 14:40:45 +0900

    refactor: EntityGraph로 변경

--- backend/src/main/java/com/cruru/applicant/domain/repository/EvaluationRepository.java
+++ backend/src/main/java/com/cruru/applicant/domain/repository/EvaluationRepository.java
@@ -5,6 +5,7 @@ import com.cruru.applicant.domain.Evaluation;
 import com.cruru.process.domain.Process;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -14,14 +15,7 @@ public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
 
     void deleteByProcessId(long processId);
 
-    @Query("""
-           SELECT e FROM Evaluation e
-           JOIN FETCH e.applicant a
-           JOIN FETCH e.process p
-           JOIN FETCH p.dashboard d
-           JOIN FETCH d.club c
-           JOIN FETCH c.member m
-           WHERE e.id = :id
-           """)
+    @EntityGraph(attributePaths = {"applicant.process.dashboard.club.member"})
+    @Query("SELECT e FROM Evaluation e WHERE e.id = :id")
     Optional<Evaluation> findByIdFetchingMember(long id);
 }
