41e3f2914198b40752d52cc5ad65d9acf2d37564
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-10-04 09:09:58 +0900

    refactor: RecursiveDropdownItem 적용

--- frontend/src/components/ApplicantModal/ApplicantBaseInfo/index.tsx
+++ frontend/src/components/ApplicantModal/ApplicantBaseInfo/index.tsx
@@ -8,7 +8,9 @@ import specificApplicant from '@hooks/useSpecificApplicant';
 import formatDate from '@utils/formatDate';
 import { useModal } from '@contexts/ModalContext';
 
-import { DropdownListItem } from '@customTypes/common';
+import RecursiveDropdownItem from '@components/_common/atoms/RecursiveDropdownItem';
+import type { DropdownItemType } from '@components/_common/atoms/RecursiveDropdownItem';
+
 import S from './style';
 
 interface ApplicantBaseInfoProps {
@@ -35,38 +37,22 @@ export default function ApplicantBaseInfo({ applicantId }: ApplicantBaseInfoProp
 
   const { applicant, process } = applicantBaseInfo;
 
-  const items = processList
-    .filter(({ processName }) => processName !== process.name)
-    .map(
-      ({ processId, processName }) =>
-        ({
-          id: processId,
-          name: processName,
-          onClick: ({ targetProcessId }: { targetProcessId: number | string }) => {
-            moveApplicantProcess({ processId: Number(targetProcessId), applicants: [applicantId] });
-          },
-        }) as DropdownListItem,
-    );
-  items.push({
-    id: 'emailButton',
-    name: '이메일 보내기',
-    hasSeparate: true,
-    onClick: () => {
-      // TODO: 이메일 보내는 로직 추가
-      alert('오픈해야함');
+  const menuItems: DropdownItemType[] = [
+    {
+      type: 'subTrigger',
+      id: 'moveProcess',
+      name: '지원자 단계 이동',
+      items: processList.map(({ processName, processId }) => ({
+        type: 'clickable',
+        id: processId,
+        name: processName,
+        onClick: ({ targetProcessId }) => {
+          moveApplicantProcess({ processId: targetProcessId, applicants: [applicantId] });
+        },
+      })),
     },
-  });
+  ];
 
-  items.push({
-    id: 'rejectButton',
-    name: '불합격 처리',
-    isHighlight: true,
-    hasSeparate: true,
-    onClick: () => {
-      // TODO: 불합격 로직 추가
-      alert('오픈해야함');
-    },
-  });
   const rejectAppHandler = () => {
     const confirmAction = (message: string, action: () => void) => {
       const isConfirmed = window.confirm(message);
@@ -90,11 +76,12 @@ export default function ApplicantBaseInfo({ applicantId }: ApplicantBaseInfoProp
         <Dropdown
           initValue={process.name}
           size="sm"
-          items={items}
           width={112}
           isShadow={false}
           disabled={applicant.isRejected}
-        />
+        >
+          <RecursiveDropdownItem items={menuItems} />
+        </Dropdown>
         <Button
           size="sm"
           color="error"
