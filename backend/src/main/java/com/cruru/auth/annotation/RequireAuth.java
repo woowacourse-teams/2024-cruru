34c90b253f5b79b6bfc9b5c579ab572a39bd4af8
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2024-10-10 02:32:22 +0900

    refactor(AuthCheckAspect): 인가 기능 리팩터링 완성

---
+++ backend/src/main/java/com/cruru/auth/annotation/RequireAuth.java
@@ -0,0 +1,14 @@
+package com.cruru.auth.annotation;
+
+import com.cruru.auth.util.SecureResource;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD,ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequireAuth {
+
+    Class<? extends SecureResource> targetDomain();
+}
