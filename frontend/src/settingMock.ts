16b7fd52bc795d032b74908dc905872aa60135ea
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-10-10 17:45:46 +0900

    feat: msw 모킹 버튼 구현

---
+++ frontend/src/settingMock.ts
@@ -0,0 +1,40 @@
+const MSW_ACTIVE_ID = 'mswActive';
+
+export default async function settingMock() {
+  if (process.env.NODE_ENV === 'development' && window.localStorage.getItem(MSW_ACTIVE_ID)) {
+    const worker = await import('@mocks/browser');
+    await worker.default.start();
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    const body = document.querySelector('body');
+    const floatingMSWActiveButton = document.createElement('button');
+
+    Object.assign(floatingMSWActiveButton.style, {
+      position: 'fixed',
+      bottom: '20px',
+      right: '80px',
+      padding: '10px 15px',
+      backgroundColor: '#007bff',
+      color: 'white',
+      border: 'none',
+      borderRadius: '5px',
+      cursor: 'pointer',
+      zIndex: '9999',
+    });
+
+    floatingMSWActiveButton.textContent = 'MSW';
+
+    floatingMSWActiveButton.addEventListener('click', () => {
+      const currentValue = window.localStorage.getItem(MSW_ACTIVE_ID);
+      if (currentValue) {
+        window.localStorage.removeItem(MSW_ACTIVE_ID);
+      } else {
+        window.localStorage.setItem(MSW_ACTIVE_ID, 'true');
+      }
+      window.location.reload();
+    });
+
+    body?.appendChild(floatingMSWActiveButton);
+  }
+}
