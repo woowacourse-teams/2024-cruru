31a1db83633e39dc49b995470dcb0d44407015fb
Seongjin Hong <hongseongjin.to@gmail.com>
2024-10-15 17:27:38 +0900

    refactor: ProcessBoard에 토글 요소 임시 적용

--- frontend/src/components/dashboard/ProcessBoard/index.tsx
+++ frontend/src/components/dashboard/ProcessBoard/index.tsx
@@ -1,21 +1,34 @@
 import { Process } from '@customTypes/process';
 import ApplicantModal from '@components/ApplicantModal';
 
+import { useMultiApplicant } from '@contexts/MultiApplicantContext';
 import ProcessColumn from '../ProcessColumn';
 import SideFloatingMessageForm from '../SideFloatingMessageForm';
 import S from './style';
+import MultiSelectToggle from '../MultiSelectToggle';
 
-interface KanbanBoardProps {
+interface ProcessBoardProps {
   processes: Process[];
   // eslint-disable-next-line react/no-unused-prop-types
   isSubTab?: boolean;
   showRejectedApplicant?: boolean;
 }
 
-export default function ProcessBoard({ processes, showRejectedApplicant = false }: KanbanBoardProps) {
+export default function ProcessBoard({ processes, showRejectedApplicant = false }: ProcessBoardProps) {
+  const { isMultiType, toggleIsMultiType, resetApplicants } = useMultiApplicant();
+
+  const handleToggleMultiType = () => {
+    if (isMultiType) resetApplicants();
+    toggleIsMultiType();
+  };
+
   return (
     <S.Container>
       {/* TODO: isSubTab을 가져와서 SubTab을 렌더링 합니다. */}
+      <MultiSelectToggle
+        onToggle={handleToggleMultiType}
+        isSelectMode={isMultiType}
+      />
       <S.ColumnWrapper>
         {processes.map((process, index) => (
           <ProcessColumn
