b0bdf6d1ce1f2e821ae75390622aa0a1f33219d3
Seongjin Hong <hongseongjin.to@gmail.com>
2024-10-15 16:18:14 +0900

    fix: DashboardHeader 스토리북의 RouterLink 에러 수정

--- frontend/src/components/dashboard/DashboardHeader/DashboardHeader.stories.tsx
+++ frontend/src/components/dashboard/DashboardHeader/DashboardHeader.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { withRouter } from 'storybook-addon-remix-react-router';
 import DashboardHeader from '.';
 
 const meta = {
@@ -15,9 +16,18 @@ const meta = {
   tags: ['autodocs'],
   argTypes: {
     title: { control: 'text' },
+    postUrl: { control: 'text' },
     startDate: { control: 'date' },
     endDate: { control: 'date' },
   },
+  decorators: [
+    withRouter,
+    (Story) => (
+      <div style={{ width: '720px' }}>
+        <Story />
+      </div>
+    ),
+  ],
 } satisfies Meta<typeof DashboardHeader>;
 
 export default meta;
@@ -26,7 +36,7 @@ type Story = StoryObj<typeof meta>;
 export const DashboardHeaderDefault: Story = {
   args: {
     title: '우아한테크코스 7기 프론트엔드 모집',
-    postUrl: '#',
+    postUrl: 'https://www.cruru.kr',
     startDate: '2024-07-15T09:00:00Z',
     endDate: '2024-12-15T09:00:00Z',
   },
