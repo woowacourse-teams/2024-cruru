3f233f7637bc87c58aacdf069a245448923f1c09
Seongjin Hong <hongseongjin.to@gmail.com>
2024-10-18 21:38:20 +0900

    feat: 필터 Popover 요소 추가

--- frontend/src/components/dashboard/DashboardFunctionTab/index.tsx
+++ frontend/src/components/dashboard/DashboardFunctionTab/index.tsx
@@ -1,11 +1,16 @@
 import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import { HiOutlineFilter } from 'react-icons/hi';
 
 import useProcess, { SimpleProcess } from '@hooks/useProcess';
 
 import InputField from '@components/_common/molecules/InputField';
+import RatingFilter from '@components/RatingFilter';
+import Popover from '@components/_common/atoms/Popover';
+
 import { useMultiApplicant } from '@contexts/MultiApplicantContext';
 import { useSearchApplicant } from '../useSearchApplicant';
+
 import ApplicantSortDropdown from '../ApplicantSortDropdown';
 import MultiSelectToggle from '../MultiSelectToggle';
 
@@ -21,8 +26,13 @@ export default function DashboardFunctionTab({ processList, onSearchName }: Dash
 
   const { debouncedName, name, updateName } = useSearchApplicant();
   const { applicants: selectedApplicantIds, isMultiType } = useMultiApplicant();
+
+  const [isFilterOpened, setIsFilterOpened] = useState<boolean>(false);
+  const filterButtonRef = useRef<HTMLButtonElement>(null);
+
   const {
     applicantSortDropdownProps: { sortOption, updateSortOption },
+    ratingFilterProps,
   } = useProcess({ dashboardId, applyFormId });
 
   useEffect(() => {
@@ -38,10 +48,33 @@ export default function DashboardFunctionTab({ processList, onSearchName }: Dash
           value={name}
           onChange={(e) => updateName(e.target.value)}
         />
+
         <ApplicantSortDropdown
           sortOption={sortOption}
           updateSortOption={updateSortOption}
         />
+
+        <S.FilterWrapper>
+          <button
+            type="button"
+            onClick={() => setIsFilterOpened(true)}
+            ref={filterButtonRef}
+          >
+            <S.FilterButtonContent>
+              <HiOutlineFilter size={12} />
+              필터
+            </S.FilterButtonContent>
+          </button>
+          <Popover
+            isOpen={isFilterOpened}
+            onClose={() => setIsFilterOpened(false)}
+            anchorEl={filterButtonRef.current}
+          >
+            <S.FilterContainer>
+              <RatingFilter {...ratingFilterProps} />
+            </S.FilterContainer>
+          </Popover>
+        </S.FilterWrapper>
       </S.FunctionsContainer>
 
       <S.FunctionsContainer>
--- frontend/src/components/dashboard/DashboardFunctionTab/style.ts
+++ frontend/src/components/dashboard/DashboardFunctionTab/style.ts
@@ -13,14 +13,40 @@ const FunctionsContainer = styled.section`
   display: flex;
   flex-direction: row;
   align-items: center;
-  gap: 1.6rem;
+  gap: 1.2rem;
 
   width: fit-content;
+`;
+
+const FilterWrapper = styled.div`
+  position: relative;
+`;
+
+const FilterButtonContent = styled.span`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.4rem;
+`;
+
+const FilterContainer = styled.div`
+  position: absolute;
+  transform: translateY(0.8rem);
+
+  background-color: ${({ theme }) => theme.baseColors.grayscale[50]};
+  border: 1px solid ${({ theme }) => theme.baseColors.grayscale[400]};
+  border-radius: 0.8rem;
+  box-shadow: ${({ theme }) => `0 0.2rem 0.6rem ${theme.baseColors.grayscale[400]}`};
+  padding: 0.2rem;
+  z-index: 10;
 `;
 
 const S = {
   Wrapper,
   FunctionsContainer,
+  FilterButtonContent,
+  FilterWrapper,
+  FilterContainer,
 };
 
 export default S;
