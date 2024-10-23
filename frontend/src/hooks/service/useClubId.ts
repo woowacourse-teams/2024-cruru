c3b867fbe07c4c561b9a4927f7eb03a4cb498eff
Kim Da Eun <midekuna@gmail.com>
2024-10-23 13:42:07 +0900

    refactor: clubId를 상태처럼 사용하도록 변경

--- frontend/src/hooks/service/useClubId.ts
+++ frontend/src/hooks/service/useClubId.ts
@@ -1,7 +1,15 @@
+import { useSyncExternalStore } from 'react';
 import ApiError from '@api/ApiError';
 import { useToast } from '@contexts/ToastContext';
 
 const CLUB_ID_KEY = 'clubId';
+
+const subscribe = (callback: () => void) => {
+  window.addEventListener('storage', callback);
+  return () => window.removeEventListener('storage', callback);
+};
+
+const getSnapshot = () => localStorage.getItem(CLUB_ID_KEY);
 
 export default function useClubId() {
   const { error } = useToast();
@@ -11,7 +19,7 @@ export default function useClubId() {
   };
 
   const getClubId = () => {
-    const clubId = localStorage.getItem(CLUB_ID_KEY);
+    const clubId = getSnapshot();
 
     if (!clubId) {
       error('세션이 만료되었습니다. 다시 로그인해주세요.');
@@ -25,9 +33,13 @@ export default function useClubId() {
     localStorage.removeItem(CLUB_ID_KEY);
   };
 
+  const clubId = useSyncExternalStore(subscribe, getSnapshot, getSnapshot);
+
   return {
     saveClubId,
     getClubId,
     clearClubId,
+    clubId,
+    isClubIdExist: !!clubId,
   };
 }
