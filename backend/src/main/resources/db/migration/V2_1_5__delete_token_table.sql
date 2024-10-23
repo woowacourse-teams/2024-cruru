bfd483999a5e260b4e2cfa3bd34025168f96b64f
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-10-23 20:36:43 +0900

    chore(flyway): v2.1.5 스키마 제거

--- backend/src/main/resources/db/migration/V2_1_5__delete_token_table.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE refresh_token
-    DROP CONSTRAINT fk_refresh_token_to_member;
-ALTER TABLE refresh_token
-    DROP CONSTRAINT uk_refresh_token_member_id;
-ALTER TABLE refresh_token
-    DROP CONSTRAINT uk_refresh_token_token;
-
-DROP TABLE IF EXISTS refresh_token;
