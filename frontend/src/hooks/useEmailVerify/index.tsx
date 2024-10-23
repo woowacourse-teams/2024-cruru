bb973f2b13af5344e8ee4967108c1656d3cb7407
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-10-23 13:23:29 +0900

    feat: useEmailVerify 훅 수정

--- frontend/src/hooks/useEmailVerify/index.tsx
+++ frontend/src/hooks/useEmailVerify/index.tsx
@@ -2,22 +2,34 @@ import emailApis from '@api/domain/email';
 import { useToast } from '@contexts/ToastContext';
 import { useMutation } from '@tanstack/react-query';
 
-export default function useEmailVerify() {
+interface UseEmailVerifyProps {
+  setIsSendVerifyEmail: (bol: boolean) => void;
+  setIsVerify: (bol: boolean) => void;
+}
+
+export default function useEmailVerify({ setIsSendVerifyEmail, setIsVerify }: UseEmailVerifyProps) {
   const { success } = useToast();
 
-  const { mutate: postVerifyEmailMutate } = useMutation({
+  const { mutate: postVerifyEmailMutate, isPending: isPendingPostVerifyEmail } = useMutation({
     mutationFn: (prop: { email: string }) => emailApis.postVerifyMail(prop),
     onSuccess: () => {
-      success('메일 전송에 성공했습니다!');
+      success('메일 전송에 성공했습니다. 이메일을 확인해주세요!');
+      setIsSendVerifyEmail(true);
     },
   });
 
-  const { mutate: confirmEmailVerifyMutate } = useMutation({
+  const { mutate: confirmEmailVerificationMutate, isPending: isPendingConfirmEmailVerification } = useMutation({
     mutationFn: (prop: { email: string; verificationCode: string }) => emailApis.confirmVerifyCode(prop),
     onSuccess: () => {
       success('이메일 인증에 성공했습니다!');
+      setIsVerify(true);
     },
   });
 
-  return { postVerifyEmailMutate, confirmEmailVerifyMutate };
+  return {
+    postVerifyEmailMutate,
+    isPendingPostVerifyEmail,
+    confirmEmailVerificationMutate,
+    isPendingConfirmEmailVerification,
+  };
 }
