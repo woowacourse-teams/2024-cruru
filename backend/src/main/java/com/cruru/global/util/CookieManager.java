96c525563ec0fece0a90b832b590fa5a464eb699
Kwoun Ki Ho <73146678+Chocochip101@users.noreply.github.com>
2024-10-24 14:42:48 +0900

    feat-be: log 커밋 revert (#934)

--- backend/src/main/java/com/cruru/global/util/CookieManager.java
+++ backend/src/main/java/com/cruru/global/util/CookieManager.java
@@ -5,11 +5,9 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CookieManager {
@@ -17,57 +15,42 @@ public class CookieManager {
     private final CookieProperties cookieProperties;
 
     public String extractAccessToken(HttpServletRequest request) {
-        log.info("Extracting access token from cookies");
         Cookie[] cookies = extractCookie(request);
         return Arrays.stream(cookies)
                 .filter(this::isAccessTokenCookie)
                 .findFirst()
                 .map(Cookie::getValue)
-                .orElseThrow(() -> {
-                    log.error("Access token cookie not found or invalid");
-                    return new IllegalCookieException();
-                });
+                .orElseThrow(IllegalCookieException::new);
     }
 
     private Cookie[] extractCookie(HttpServletRequest request) {
-        log.debug("Extracting cookies from request");
         Cookie[] cookies = request.getCookies();
 
         if (cookies == null) {
-            log.warn("No cookies found in the request");
             throw new IllegalCookieException();
         }
         return cookies;
     }
 
     private boolean isAccessTokenCookie(Cookie cookie) {
-        boolean result = cookieProperties.accessTokenKey().equals(cookie.getName());
-        log.debug("Is access token cookie: {}", result);
-        return result;
+        return cookieProperties.accessTokenKey().equals(cookie.getName());
     }
 
     public String extractRefreshToken(HttpServletRequest request) {
-        log.info("Extracting refresh token from cookies");
         Cookie[] cookies = extractCookie(request);
         return Arrays.stream(cookies)
                 .filter(this::isRefreshTokenCookie)
                 .findFirst()
                 .map(Cookie::getValue)
-                .orElseThrow(() -> {
-                    log.error("Refresh token cookie not found or invalid");
-                    return new IllegalCookieException();
-                });
+                .orElseThrow(IllegalCookieException::new);
     }
 
     private boolean isRefreshTokenCookie(Cookie cookie) {
-        boolean result = cookieProperties.refreshTokenKey().equals(cookie.getName());
-        log.debug("Is refresh token cookie: {}", result);
-        return result;
+        return cookieProperties.refreshTokenKey().equals(cookie.getName());
     }
 
     public ResponseCookie createAccessTokenCookie(String token) {
-        log.info("Creating access token cookie");
-        ResponseCookie cookie = ResponseCookie.from(cookieProperties.accessTokenKey(), token)
+        return ResponseCookie.from(cookieProperties.accessTokenKey(), token)
                 .httpOnly(cookieProperties.httpOnly())
                 .secure(cookieProperties.secure())
                 .domain(cookieProperties.domain())
@@ -75,13 +58,10 @@ public class CookieManager {
                 .sameSite(cookieProperties.sameSite())
                 .maxAge(cookieProperties.maxAge())
                 .build();
-        log.debug("Access token cookie created: {}", cookie);
-        return cookie;
     }
 
     public ResponseCookie createRefreshTokenCookie(String refreshToken) {
-        log.info("Creating refresh token cookie");
-        ResponseCookie cookie = ResponseCookie.from(cookieProperties.refreshTokenKey(), refreshToken)
+        return ResponseCookie.from(cookieProperties.refreshTokenKey(), refreshToken)
                 .httpOnly(cookieProperties.httpOnly())
                 .secure(cookieProperties.secure())
                 .domain(cookieProperties.domain())
@@ -89,13 +69,10 @@ public class CookieManager {
                 .sameSite(cookieProperties.sameSite())
                 .maxAge(cookieProperties.maxAge())
                 .build();
-        log.debug("Refresh token cookie created: {}", cookie);
-        return cookie;
     }
 
     public ResponseCookie clearAccessTokenCookie() {
-        log.info("Clearing access token cookie");
-        ResponseCookie cookie = ResponseCookie.from(cookieProperties.accessTokenKey())
+        return ResponseCookie.from(cookieProperties.accessTokenKey())
                 .httpOnly(cookieProperties.httpOnly())
                 .secure(cookieProperties.secure())
                 .domain(cookieProperties.domain())
@@ -103,13 +80,10 @@ public class CookieManager {
                 .sameSite(cookieProperties.sameSite())
                 .maxAge(0)
                 .build();
-        log.debug("Access token cookie cleared: {}", cookie);
-        return cookie;
     }
 
     public ResponseCookie clearRefreshTokenCookie() {
-        log.info("Clearing refresh token cookie");
-        ResponseCookie cookie = ResponseCookie.from(cookieProperties.refreshTokenKey())
+        return ResponseCookie.from(cookieProperties.refreshTokenKey())
                 .httpOnly(cookieProperties.httpOnly())
                 .secure(cookieProperties.secure())
                 .domain(cookieProperties.domain())
@@ -117,7 +91,5 @@ public class CookieManager {
                 .sameSite(cookieProperties.sameSite())
                 .maxAge(0)
                 .build();
-        log.debug("Refresh token cookie cleared: {}", cookie);
-        return cookie;
     }
 }
