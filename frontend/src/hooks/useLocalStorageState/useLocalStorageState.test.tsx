c709bcda61f68aaaafa038530e86ae87627f70e4
Jeongwoo Park <wahoo9040@naver.com>
2024-12-18 22:54:09 +0900

    test: useLocalStorageState 테스트 케이스 추가

--- frontend/src/hooks/useLocalStorageState/useLocalStorageState.test.tsx
+++ frontend/src/hooks/useLocalStorageState/useLocalStorageState.test.tsx
@@ -5,7 +5,6 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import useLocalStorageState from '.';
 import '@testing-library/jest-dom';
 
-// 1. 원시값을 테스트하는 컴포넌트
 const PrimitiveValueComponent = () => {
   const [value, setValue] = useLocalStorageState<number>('primitiveKey', 0);
 
@@ -13,12 +12,12 @@ const PrimitiveValueComponent = () => {
     <div>
       <p>Value: {value}</p>
       <button onClick={() => setValue(value + 1)}>Increment</button>
+      <button onClick={() => setValue((prev) => prev + 1)}>Increment2</button>
       <button onClick={() => setValue(0)}>Reset</button>
     </div>
   );
 };
 
-// 2. 객체를 테스트하는 컴포넌트
 const ObjectValueComponent = () => {
   const [user, setUser] = useLocalStorageState<{ name: string; age: number }>('objectKey', {
     name: 'lurgi',
@@ -31,6 +30,7 @@ const ObjectValueComponent = () => {
         Name: {user.name}, Age: {user.age}
       </p>
       <button onClick={() => setUser({ ...user, age: user.age + 1 })}>Increase Age</button>
+      <button onClick={() => setUser((prev) => ({ ...prev, age: prev.age + 1 }))}>Increase Age2</button>
     </div>
   );
 };
@@ -40,16 +40,22 @@ describe('useLocalStorageState의 값이 원시값인 경우에 대한 테스트
     window.localStorage.clear();
   });
 
-  // 1. 원시값 테스트
   describe('Primitive Value Tests', () => {
     it('초기 상태를 설정한다', () => {
       render(<PrimitiveValueComponent />);
       expect(screen.getByText('Value: 0')).toBeInTheDocument();
     });
 
-    it('상태 변경 시 localStorage에 저장한다', () => {
+    it('[setState 인자 원시값] 상태 변경 시 localStorage에 저장한다', () => {
       render(<PrimitiveValueComponent />);
       fireEvent.click(screen.getByText('Increment'));
+      expect(screen.getByText('Value: 1')).toBeInTheDocument();
+      expect(window.localStorage.getItem('primitiveKey')).toBe('1');
+    });
+
+    it('[setState 인자 함수] 상태 변경 시 localStorage에 저장한다', () => {
+      render(<PrimitiveValueComponent />);
+      fireEvent.click(screen.getByText('Increment2'));
       expect(screen.getByText('Value: 1')).toBeInTheDocument();
       expect(window.localStorage.getItem('primitiveKey')).toBe('1');
     });
@@ -58,6 +64,8 @@ describe('useLocalStorageState의 값이 원시값인 경우에 대한 테스트
       window.localStorage.setItem('primitiveKey', '10');
       render(<PrimitiveValueComponent />);
       expect(screen.getByText('Value: 10')).toBeInTheDocument();
+      fireEvent.click(screen.getByText('Increment2'));
+      expect(window.localStorage.getItem('primitiveKey')).toBe('11');
     });
   });
 
@@ -67,7 +75,7 @@ describe('useLocalStorageState의 값이 원시값인 경우에 대한 테스트
       expect(screen.getByText('Name: lurgi, Age: 30')).toBeInTheDocument();
     });
 
-    it('상태 변경 시 localStorage에 저장한다', () => {
+    it('[setState 인자 원시값] 상태 변경 시 localStorage에 저장한다', () => {
       render(<ObjectValueComponent />);
       fireEvent.click(screen.getByText('Increase Age'));
       expect(screen.getByText('Name: lurgi, Age: 31')).toBeInTheDocument();
@@ -76,12 +84,23 @@ describe('useLocalStorageState의 값이 원시값인 경우에 대한 테스트
       expect(storedValue).toBe(JSON.stringify({ name: 'lurgi', age: 31 }));
     });
 
+    it('[setState 인자 함수] 상태 변경 시 localStorage에 저장한다', () => {
+      render(<ObjectValueComponent />);
+      fireEvent.click(screen.getByText('Increase Age2'));
+      expect(screen.getByText('Name: lurgi, Age: 31')).toBeInTheDocument();
+
+      const storedValue = window.localStorage.getItem('objectKey');
+      expect(storedValue).toBe(JSON.stringify({ name: 'lurgi', age: 31 }));
+    });
+
     it('localStorage에 값이 있으면 초기 상태로 사용한다', () => {
-      const storedObject = JSON.stringify({ name: 'Jane Doe', age: 28 });
+      const storedObject = JSON.stringify({ name: 'jeong woo', age: 28 });
       window.localStorage.setItem('objectKey', storedObject);
 
       render(<ObjectValueComponent />);
-      expect(screen.getByText('Name: Jane Doe, Age: 28')).toBeInTheDocument();
+      expect(screen.getByText('Name: jeong woo, Age: 28')).toBeInTheDocument();
+      fireEvent.click(screen.getByText('Increase Age2'));
+      expect(screen.getByText('Name: jeong woo, Age: 29')).toBeInTheDocument();
     });
   });
 });
