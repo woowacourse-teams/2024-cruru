60c5c6d31fd74add17037f7eaa151358f9df7312
Jeongwoo Park <wahoo9040@naver.com>
2024-12-19 16:17:38 +0900

    feat: enableStorage 옵션 추가

--- frontend/src/hooks/useLocalStorageState/index.ts
+++ frontend/src/hooks/useLocalStorageState/index.ts
@@ -1,13 +1,24 @@
 import { useState } from 'react';
 
+interface OptionProp {
+  enableStorage: boolean;
+}
+
 /**
  * useLocalStorageState
  * @param key - LocalStorage에 저장될 키 값
  * @param initialValue - 초기 상태 값
+ * @param option - { use: boolean }
  * @returns [상태 값, 상태를 변경하는 함수] useState의 반환값과 동일합니다.
  */
-function useLocalStorageState<T>(key: string, initialValue: T): [T, (value: T | ((prev: T) => T)) => void] {
+function useLocalStorageState<T>(
+  key: string,
+  initialValue: T,
+  option: OptionProp = { enableStorage: true },
+): [T, (value: T | ((prev: T) => T)) => void] {
   const [state, _setState] = useState<T>(() => {
+    if (!option.enableStorage) return initialValue;
+
     try {
       const storedValue = window.localStorage.getItem(key);
       return storedValue !== null ? JSON.parse(storedValue) : initialValue;
