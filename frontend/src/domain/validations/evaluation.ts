46c9234c8480d1b08587499de2a2395da81f8e25
Seongjin Hong <mail@seongjin.me>
2025-01-07 23:12:56 +0900

    feat: 평가자 이름 필드에 대한 validation 코드 구현

--- frontend/src/domain/validations/evaluation.ts
+++ frontend/src/domain/validations/evaluation.ts
@@ -1,5 +1,42 @@
-import { EVALUATION_CONTENT_MAX_LENGTH } from '@components/ApplicantModal/ApplicantEvalInfo/constants';
+import {
+  EVALUATION_CONTENT_MAX_LENGTH,
+  EVALUATION_EVALUATOR_MAX_LENGTH,
+} from '@components/ApplicantModal/ApplicantEvalInfo/constants';
 import ValidationError from '@utils/errors/ValidationError';
+
+export const validateEvaluator = (evaluator: string) => {
+  if (evaluator.length > EVALUATION_EVALUATOR_MAX_LENGTH) {
+    throw new ValidationError({
+      inputName: 'evaluator',
+      message: `평가자 이름은 ${EVALUATION_EVALUATOR_MAX_LENGTH}자 이내로 입력해주세요.`,
+    });
+  }
+
+  const trimmedEvaluator = evaluator.trim();
+
+  if (evaluator.length > 0 && trimmedEvaluator.length === 0) {
+    throw new ValidationError({
+      inputName: 'evaluator',
+      message: '평가자 이름에 공백만 입력하는 것은 허용되지 않습니다.',
+    });
+  }
+
+  if (evaluator.length > 0 && evaluator.length !== trimmedEvaluator.length) {
+    throw new ValidationError({
+      inputName: 'evaluator',
+      message: '평가자 이름 앞뒤의 공백은 허용되지 않습니다.',
+    });
+  }
+
+  const allowedEvaluatorPattern = /^[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣]+([ ]?[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣]+)*$/;
+
+  if (trimmedEvaluator.length > 0 && !allowedEvaluatorPattern.test(trimmedEvaluator)) {
+    throw new ValidationError({
+      inputName: 'name',
+      message: "한글/영문과 공백, 하이픈(-), 홑따옴표('), 마침표(.)만 허용됩니다.",
+    });
+  }
+};
 
 export const validateEvalContent = (title: string) => {
   if (title.length > EVALUATION_CONTENT_MAX_LENGTH) {
