e69081be9751d97c738ea8e33e2d7a13a75f340a
Seongjin Hong <mail@seongjin.me>
2025-01-08 22:38:35 +0900

    feat: useEvaluationMutation 훅을 API method 별로 분리하여 재구성

--- frontend/src/hooks/useEvaluationMutation/index.ts
+++ frontend/src/hooks/useEvaluationMutation/index.ts
@@ -3,34 +3,54 @@ import { useParams } from 'react-router-dom';
 import evaluationApis from '@api/domain/evaluation';
 import QUERY_KEYS from '@hooks/queryKeys';
 
-interface UseEvaluationMutationParams {
+interface DefaultMutationParams {
   processId: number;
   applicantId: number;
+}
+
+interface UseCreateEvaluationMutationParams extends DefaultMutationParams {
   closeOnSuccess: () => void;
 }
 
-interface MutationParams {
-  processId: number;
-  applicantId: number;
+interface CreateMutationParams extends DefaultMutationParams {
   evaluator: string;
   score: number;
   content: string;
 }
 
-export default function useEvaluationMutation({ processId, applicantId, closeOnSuccess }: UseEvaluationMutationParams) {
+function useEvaluationQueryInvalidation() {
   const queryClient = useQueryClient();
   const { dashboardId, applyFormId } = useParams() as { dashboardId: string; applyFormId: string };
 
-  const invalidateQueries = () => {
+  return (processId: number, applicantId: number) => {
     queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.EVALUATION, processId, applicantId] });
     queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.DASHBOARD, dashboardId, applyFormId] });
   };
+}
+
+export function useCreateEvaluationMutation({
+  processId,
+  applicantId,
+  closeOnSuccess,
+}: UseCreateEvaluationMutationParams) {
+  const invalidateQueries = useEvaluationQueryInvalidation();
 
   return useMutation({
-    mutationFn: (params: MutationParams) => evaluationApis.create(params),
+    mutationFn: (params: CreateMutationParams) => evaluationApis.create(params),
     onSuccess: () => {
-      invalidateQueries();
+      invalidateQueries(processId, applicantId);
       closeOnSuccess();
+    },
+  });
+}
+
+export function useDeleteEvaluationMutation({ processId, applicantId }: DefaultMutationParams) {
+  const invalidateQueries = useEvaluationQueryInvalidation();
+
+  return useMutation({
+    mutationFn: ({ evaluationId }: { evaluationId: number }) => evaluationApis.delete({ evaluationId }),
+    onSuccess: () => {
+      invalidateQueries(processId, applicantId);
     },
   });
 }
