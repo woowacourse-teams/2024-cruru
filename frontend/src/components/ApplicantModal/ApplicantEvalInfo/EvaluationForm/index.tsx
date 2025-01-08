c207be2034892f4a70e8d8e6a7b826757356170d
Seongjin Hong <mail@seongjin.me>
2025-01-08 19:40:20 +0900

    refactor: 입력 취소 버튼의 텍스트 변경

--- frontend/src/components/ApplicantModal/ApplicantEvalInfo/EvaluationForm/index.tsx
+++ frontend/src/components/ApplicantModal/ApplicantEvalInfo/EvaluationForm/index.tsx
@@ -95,7 +95,7 @@ export default function EvaluationForm({ processId, applicantId, onClose }: Eval
           onClick={onClose}
           disabled={isPending}
         >
-          입력 취소하기
+          입력 취소
         </S.CancelButton>
       </S.Header>
 
